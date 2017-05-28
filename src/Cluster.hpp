1b84ecde388f200470c4c31eb6292131d4b55b87
Mattias Ångqvist <angqvist@chalmers.se>
2017-05-28 23:31:55 +0200

    WIP: Sorting clusters...

--- src/Cluster.hpp
+++ src/Cluster.hpp
@@ -8,6 +8,7 @@
 #include <pybind11/stl.h>
 #include <map>
 #include <unordered_map>
+#include <algorithm>
 using namespace Eigen;
 
 namespace py = pybind11;
@@ -49,9 +50,84 @@ class Cluster
         return _distances;
     }
 
-    // comparison operator for sortable clusters
+    void sortCluster()
+    {
+        std::vector<std::vector<double>> first_dists;
 
-   friend bool operator<(const Cluster &c1, const Cluster &c2)
+        for(int i  = 0; i < _sites.size(); i++)
+        {
+            first_dists.push_back(getDistsToSite(i));
+        }
+        //ordering
+
+
+    }
+
+    /*
+    Get all dists that origin from site i
+
+    Sorted double vector
+    */
+    std::vector<double> getDistsToSite(int i)
+    {
+        std::vector<double> dists;
+        int counter = 0;
+        for(int k; k < _sites.size(); k++)
+        {
+            for(int l=k; l < _sites.size(); l++)
+            {
+                if (k== i or l == i)
+                {
+                    dists.push_back(_distances[counter]);
+                }
+                counter++;                
+            }
+        }
+        
+        std::sort(dists.begin(), dists.end());
+        return dists;
+    }
+
+    /*
+    Swap sites of site i and site j and also changes the corresponding distances.
+    */
+    void swapSites(const int i, const int j)
+    {   
+        //swap sites
+        std::iter_swap(_sites.begin() + i, _sites.begin() + j);
+
+        std::vector<std::tuple<int,int, double>> dist_indices;
+        int counter = 0;
+        for(int k; k < _sites.size(); k++)
+        {
+            for(int l=k; l < _sites.size(); l++)
+            {
+                int first = k;
+                int second = l;
+                if( first == i){ first = j; }
+                if( first == j){ first = i; }
+                if( second == i){ second = j; }
+                if( second == j){ second = i; }
+                
+                if( second < first)
+                {
+                    auto temp = first;
+                    first = second;
+                    second = temp;                    
+                 }
+                dist_indices.push_back(std::make_tuple(first, second, _distances[counter++]));
+            }
+        }
+        std::sort(dist_indices.begin(), dist_indices.end());
+        counter = 0;
+        for(const auto &tup : dist_indices)
+        {
+            _distances[counter++] = std::get<2>(tup);
+        }
+    }
+
+    // comparison operator for sortable clusters
+    friend bool operator<(const Cluster &c1, const Cluster &c2)
     {
         //1) compare number of bodies in cluster
         if (c1._sites.size() < c2._sites.size())
@@ -62,6 +138,7 @@ class Cluster
         {
             return false;
         }
+
         //2) compare distances
         for (int i = 0; i < c1._distances.size(); i++)
         {
