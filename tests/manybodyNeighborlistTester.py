8698a5d6ff146dedd8fb1696e45ae0d506c7d9d9
angqvist <angqvist@chalmers.se>
2017-07-26 09:41:57 +0200

    MAINT: removed old commented code

--- tests/manybodyNeighborlistTester.py
+++ tests/manybodyNeighborlistTester.py
@@ -57,12 +57,6 @@ class manybodyNeighborlistTester():
         for j in Ni:
             originalNeighborCopy = current_original_neighbors.copy()
 
-            # if self.nbrCompare(j, originalNeighborCopy[-1]) and (len(originalNeighborCopy) == 1 and not bothways):
-            #    continue
-            # print(len(originalNeighborCopy) == 1 , self.nbrCompare(j,
-            # originalNeighborCopy[-1]),bothways, (len(originalNeighborCopy) ==
-            # 1 and (not bothways and self.nbrCompare(j,
-            # originalNeighborCopy[-1])))
             if len(originalNeighborCopy) == 1 and (not bothways and self.nbrCompare(j, originalNeighborCopy[-1])):
                 continue
 
