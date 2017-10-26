c680eebd05ec1aa135e9f8d82e43a491bc16460f
williamz <william.munoz@chalmers.se>
2017-10-26 10:52:25 +0200

    BUG: fixed test atoms in database returning None

--- tests/gen_structure_database.py
+++ tests/gen_structure_database.py
@@ -11,11 +11,12 @@ Some tests may use partially the following database.
 """ FCC (single element, primitive cell, pbc=True) """
 atoms_1 = bulk('Al','fcc', a=1.0)
 
-""" FCC (single element, cubic structure, pbc=True) """
+""" FCC (single element, supercell, pbc=True) """
 atoms_2 = bulk('Al', 'fcc', a=1.0).repeat(2)
 
 """ FCC (single element, distorted structure, pbc=True) """
-atoms_3 = bulk('Al', 'fcc', a=1.0).repeat(2).rattle(stdev=0.01, seed=42)
+atoms_3 = bulk('Al', 'fcc', a=1.0).repeat(2)
+atoms_3.rattle(stdev=0.001, seed=42)
 
 """ BCC (two elements, cubic structure, pbc=True) """
 atoms_4 = bulk('Ti','bcc', a=1.0).repeat(2)
@@ -24,16 +25,16 @@ for atom in atoms_4:
         atom.symbol='W'
 
 """ rocksalt (two elements, cubic structure) """
-atoms_5 = bulk('NaCl', 'rocksalt', a=1.0).repeat(1)
+atoms_5 = bulk('NaCl', 'rocksalt', a=1.0)
 
 """ HCP (single element, hexagonal structure) """
-atoms_6 = bulk('Ni', 'hcp', a=0.625, c=1.0).repeat(1)
+atoms_6 = bulk('Ni', 'hcp', a=0.625, c=1.0)
 
 """ perovskite (three elements, cubic structure) """
 a=1.0
 b=a/2
 atoms_7 = Atoms('BaZrO3', positions=[(0, 0, 0), (b,b,b), 
-                (b,b,0), (b,0,b), (0,b,b)], cell=[a, a, a], pbc=True).repeat(1)
+                (b,b,0), (b,0,b), (0,b,b)], cell=[a, a, a], pbc=True)
 
 """ surface slab (two elements, pbc=[True, True, False]) """
 atoms_8 = fcc111('Pd', a=1.0, size=(2,2,3))
@@ -50,7 +51,7 @@ db = connect('structures_for_testing.db')
 db.write(atoms_1, tag='Al-fcc-primitive_cell')
 db.write(atoms_2, tag='Al-fcc-supercell')
 db.write(atoms_3, tag='Al-fcc-distorted')
-db.write(atoms_4, tag='WTi-bcc-cubic_cell')
+db.write(atoms_4, tag='WTi-bcc-supercell')
 db.write(atoms_5, tag='NaCl-rocksalt-cubic-cell')
 db.write(atoms_6, tag='Ni-hcp-hexagonal-cell')
 db.write(atoms_7, tag='BaZrO3-perovskite')
