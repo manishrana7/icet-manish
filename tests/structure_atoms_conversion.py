7064f84ae7311fb7f84fbfea87bcb9d5c0d8ba6d
Paul Erhart <erhart@chalmers.se>
2017-11-02 10:34:52 +0100

    TEST: fixed and cleaned up structure conversion test

--- tests/structure_atoms_conversion.py
+++ tests/structure_atoms_conversion.py
@@ -1,45 +1,50 @@
-from ase import Atoms
 from ase.db import connect
-from ase.io import read
-import numpy as np
-from icetdev import *
-from icetdev.structure import *
+import icetdev as icet
 
-"""
-Testing icet structure against ASE 
+'''
+Testing icet structure against ASE
+'''
 
-"""
-
-""" Set up structures from database """
-db = connect("structures_for_testing.db")
+''' Fetch structures from database '''
+db = connect('structures_for_testing.db')
 
 for row in db.select():
 
     atoms_row = row.toatoms()
 
-    """ Convert ASE atoms to icet structures """
-    structure = structure_from_atoms(atoms_row)
+    ''' Convert ASE atoms to icet structures '''
+    structure = icet.structure_from_atoms(atoms_row)
 
-    """ Test that each position is equal """
-    assert len(atoms_row.positions) == len(structure.get_positions()), "Test for len of positions failed for structure {}".format(raw.tag) 
-    
-    for ase_pos, struct_pos in zip(atoms_row.positions, structure.get_positions()):
-        assert (ase_pos == struct_pos).all(), "Test for positions failed for structure {}".format(row.tag)
+    ''' Test that each position is equal '''
+    msg = 'Test for len of positions failed for structure {}'.format(row.tag)
+    assert len(atoms_row) == len(structure.get_positions()), msg
 
-    """ Test that each element is equal """
-    assert len(atoms_row.get_chemical_symbols()) == len(structure.get_elements()), "Test for len of elements failed for for structure {}".format(atoms_row.tag)
-    
+    for ase_pos, struct_pos in zip(atoms_row.positions,
+                                   structure.get_positions()):
+        msg = 'Test for positions failed for structure {}'.format(row.tag)
+        assert (ase_pos == struct_pos).all(), msg
+
+    ''' Test that each element is equal '''
+    msg = 'Test for len of elements failed for structure {}'.format(row.tag)
+    assert len(atoms_row) == len(structure.get_elements()), msg
+
     for ase_symbol, struct_symbol in zip(atoms_row.get_atomic_numbers(),
                                          structure.get_elements()):
-        assert ase_symbol == struct_symbol,"Test for elements failed for structure {} when {} don't match {}".format(row.tag, ase_symbol, struct_symbol)
+        msg = 'Test for elements failed for structure {}'.format(row.tag)
+        msg += '; {} != {}'.format(ase_symbol, struct_symbol)
+        assert ase_symbol == struct_symbol, msg
 
-    """ Test pbc equality """
-    assert (structure.pbc == atoms_row.pbc).all(), "Test for periodic boundary conditions failed for structure {}".format(row.tag)
+    ''' Test periodic boundary conditions '''
+    msg = 'Test for periodic boundary conditions failed'
+    msg += ' for structure {}'.format(row.tag)
+    assert (structure.pbc == atoms_row.pbc).all(), msg
 
-    """ Test unit cell equality """
-    assert (structure.cell == atoms_row.cell).all(), "Test for cell failed for structure {}".format(row.tag)
+    ''' Test unit cell equality '''
+    msg = 'Test for cell failed for structure {}'.format(row.tag)
+    assert (structure.cell == atoms_row.cell).all(), msg
 
-    """ Assert that structure return as an equal ASE atoms object """
+    ''' Assert that structure return as an equal ASE atoms object '''
     atoms_icet = structure.to_atoms()
-    assert atoms_icet == atoms_row, "Test for conversion back to atoms failed for structure {}".format(row.tag)
-
+    msg = 'Test for conversion back to atoms failed'
+    msg += ' for structure {}'.format(row.tag)
+    assert atoms_icet == atoms_row, msg
