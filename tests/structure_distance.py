d2a0d9cfb33395e08d98ae8a10addf97d7a76533
Paul Erhart <erhart@chalmers.se>
2017-11-03 10:55:41 +0100

    TEST: fixed and cleaned up structure distance test

--- tests/structure_distance.py
+++ tests/structure_distance.py
@@ -1,26 +1,31 @@
-from icetdev import structure
-from ase import Atoms
 import numpy as np
 from ase.db import connect
-from icetdev import *
-from icetdev.structure import *
+import icetdev as icet
 from ase.neighborlist import NeighborList
 
-""" Distance tolerance for comparing distances """ 
+'''
+Testing the calculation of distances with offsets
+'''
+
+
+''' Tolerance for comparing distances '''
 DISTTOL = 1e-8
 
-""" Test distance calculator with offsets """
-
-db = connect("structures_for_testing.db")
+''' Fetch structures from database '''
+db = connect('structures_for_testing.db')
 
 for row in db.select():
     atoms_row = row.toatoms()
-    structure = structure_from_atoms(atoms_row)
+    structure = icet.structure_from_atoms(atoms_row)
     nl = NeighborList(len(atoms_row)*[2.6])
     nl.update(atoms_row)
     for index in range(len(atoms_row)):
         indices, offsets = nl.get_neighbors(index)
         for i, offset in zip(indices, offsets):
-            dist = np.linalg.norm(atoms_row.positions[index] - (atoms_row.positions[i] + np.dot(offset, atoms_row.get_cell())))
-            dist_struct = structure.get_distance2(index, [0,0,0], i, offset)
-            assert (dist - dist_struct) < DISTTOL, "Testing distance calculator failed for structure {}".format(row.tag)
+            dvec = atoms_row.positions[index] - atoms_row.positions[i]
+            dvec -= np.dot(offset, atoms_row.get_cell())
+            dist_ase = np.linalg.norm(dvec)
+            dist_struct = structure.get_distance2(index, [0, 0, 0], i, offset)
+            msg = 'Testing distance calculator failed'
+            msg += ' for structure {}'.format(row.tag)
+            assert dist_ase - dist_struct < DISTTOL, msg
