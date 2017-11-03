36f53a34a8dd2fefb7272792332200fa4d2439cc
Paul Erhart <erhart@chalmers.se>
2017-11-03 17:03:37 +0100

    TEST: cleaned up some tests

--- tests/test_cluster-vectors-bcc.py
+++ tests/test_cluster-vectors-bcc.py
@@ -1,9 +1,8 @@
-"""
+'''
 This scripts checks the computation of cluster vectors for three BCC-based
 structures.
-"""
+'''
 
-from icetdev import clusterspace
 from icetdev.clusterspace import create_clusterspace
 from icetdev.structure import structure_from_atoms
 from ase.build import bulk, make_supercell
@@ -17,19 +16,18 @@ subelements = ['W', 'Ti']
 prototype = bulk('W')
 
 
-clusterspace = create_clusterspace(prototype, cutoffs, subelements)
+cs = create_clusterspace(prototype, cutoffs, subelements)
 
 conf = structure_from_atoms(prototype.copy())
 
 print('Structure no. 1 (nat= {}):'.format(len(conf)))
-cv = clusterspace.get_clustervector(conf)
+cv = cs.get_clustervector(conf)
 cv_target = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
                      1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
                      1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
                      1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
                      1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
                      1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
-print(cv)
 assert np.all(np.abs(cv_target - cv) < 1e-6)
 
 conf = make_supercell(prototype, [[2, 0, 1],
@@ -40,16 +38,15 @@ conf[0].symbol = 'Ti'
 conf[1].symbol = 'Ti'
 conf = structure_from_atoms(conf)
 print('Structure no. 2 (nat= {}):'.format(len(conf)))
-cv = clusterspace.get_clustervector(conf)
+cv = cs.get_clustervector(conf)
 
 cv_target = np.array([1.0, 0.0, 0.0, -0.3333333333333333,
                      -0.3333333333333333, 0.0, 1.0, 1.0,
-                     0.0, -0.3333333333333333, 
+                     0.0, -0.3333333333333333,
                      -0.3333333333333333, 0.0, 0.0, 0.0,
                      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-print(cv)
 assert np.all(np.abs(cv_target - cv) < 1e-6)
 
 conf = make_supercell(prototype, [[1,  0, 1],
@@ -61,7 +58,7 @@ conf[1].symbol = 'Ti'
 conf[2].symbol = 'Ti'
 print('Structure no. 3 (nat= {}):'.format(len(conf)))
 conf = structure_from_atoms(conf)
-cv = clusterspace.get_clustervector(conf)
+cv = cs.get_clustervector(conf)
 cv_target = np.array([1.0, -0.5, 0.0, 0.6666666666666666,
                       0.3333333333333333, 0.0, 0.0, 1.0,
                       0.0, 0.6666666666666666, 0.3333333333333333,
@@ -73,5 +70,4 @@ cv_target = np.array([1.0, -0.5, 0.0, 0.6666666666666666,
                       -0.5, 0.5, -0.16666666666666666, 0.5,
                       0.16666666666666666, -0.16666666666666666,
                       -0.5, 0.5, 0.16666666666666666, -0.5])
-print(cv)
 assert np.all(np.abs(cv_target - cv) < 1e-6)
--- tests/test_clustervectors_with_different_Mi.py
+++ tests/test_clustervectors_with_different_Mi.py
@@ -1,12 +1,11 @@
-"""
-This scripts checks the computation of clustervectors with non constant Mi
-
-"""
+'''
+This scripts checks the computation of cluster vectors with a variable number
+of components on different sites.
+'''
 
 import numpy as np
 
 import icetdev
-from ase.build import bulk
 from ase.db import connect
 from icetdev.clusterspace import create_clusterspace, get_singlet_info
 from icetdev.structure import structure_from_atoms
@@ -15,7 +14,9 @@ print(__doc__)
 
 
 def test_mi_int_list_and_dict(atoms, subelements, cutoffs, allowed_sites):
-    """Test that int Mi, list Mi and dict Mi produces the same clustervectors"""
+    '''
+    Test that int Mi, list Mi and dict Mi produces the same clustervectors.
+    '''
 
     prim_size = len(
         icetdev.permutationMap.__get_primitive_structure(atoms.copy()))
@@ -26,13 +27,16 @@ def test_mi_int_list_and_dict(atoms, subelements, cutoffs, allowed_sites):
 
     singlet_data = get_singlet_info(atoms.copy())
     for singlet in singlet_data:
-        Mi_dict[singlet["orbit_index"]] = allowed_sites
+        Mi_dict[singlet['orbit_index']] = allowed_sites
 
-    clusterspace_int = create_clusterspace(atoms, cutoffs, subelements, Mi=Mi_int)
+    clusterspace_int = create_clusterspace(atoms, cutoffs, subelements,
+                                           Mi=Mi_int)
 
-    clusterspace_list = create_clusterspace(atoms, cutoffs, subelements, Mi=Mi_list)
+    clusterspace_list = create_clusterspace(atoms, cutoffs, subelements,
+                                            Mi=Mi_list)
 
-    clusterspace_dict = create_clusterspace(atoms, cutoffs, subelements, Mi=Mi_dict)
+    clusterspace_dict = create_clusterspace(atoms, cutoffs, subelements,
+                                            Mi=Mi_dict)
 
     atoms_prim = clusterspace_int.get_primitive_structure().to_atoms()
 
@@ -51,27 +55,28 @@ def test_mi_int_list_and_dict(atoms, subelements, cutoffs, allowed_sites):
     cv_list = clusterspace_list.get_clustervector(conf)
     cv_dict = clusterspace_dict.get_clustervector(conf)
 
-    assert (np.array(cv_int) == np.array(
-        cv_list)).all, "Error: clustervectors were not equal for an equivalent Mi with: type(Mi) == int and type(Mi)==list"
-    assert (np.array(cv_int) == np.array(
-        cv_dict)).all, "Error: clustervectors were not equal for an equivalent Mi with: type(Mi) == int and type(Mi)==dict"
+    msg = 'Cluster vectors were not equal for an equivalent Mi (int vs list)'
+    assert (np.array(cv_int) == np.array(cv_list)).all, msg
+
+    msg = 'Cluster vectors were not equal for an equivalent Mi (int vs dict)'
+    assert (np.array(cv_int) == np.array(cv_dict)).all, msg
 
 
-db = connect("structures_for_testing.db")
+db = connect('structures_for_testing.db')
 
-subelements = ["H", "He", "Pb"]
+subelements = ['H', 'He', 'Pb']
 for row in db.select():
     atoms_row = row.toatoms()
     atoms_tag = row.tag
     cutoffs = [1.4] * 3
     if len(atoms_row) == 0:
         continue
-    if atoms_row.get_pbc().all() == True:
+    if atoms_row.get_pbc().all():
         atoms_row.wrap()
     if True:  # atoms_row.get_pbc().all() == True:
-        print("Testing finding cv for structure: {} with cutoffs {}".format(
+        print('Testing finding cv for structure: {} with cutoffs {}'.format(
             atoms_tag, cutoffs))
         atoms_row.set_chemical_symbols(len(atoms_row) * [atoms_row[0].symbol])
-
-        for allowed_sites in range(2,4):
-            test_mi_int_list_and_dict(atoms_row, subelements, cutoffs,allowed_sites)
+        for allowed_sites in range(2, 4):
+            test_mi_int_list_and_dict(atoms_row, subelements,
+                                      cutoffs, allowed_sites)
