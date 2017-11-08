26c51c569dd4f7eeaa6071c90c5acfffa61c102a
Paul Erhart <erhart@chalmers.se>
2017-11-08 04:57:32 +0100

    MAINT: cleaned up __init__.py and added basic module information

--- icetdev/__init__.py
+++ icetdev/__init__.py
@@ -1,15 +1,27 @@
 from __future__ import print_function, division
+from .structure import Structure
+from .clusterspace import ClusterSpace
+from .cluster_expansion import ClusterExpansion
 
-# Make sure native extension module was built against the same  version of the
-# Python interpreter that is executing this file. This check serves to detect
-# mistakes made by the user, who might be executing this script with a
-# different interpreter than the one used to build.
+'''
+icet module
+'''
 
-from .structure import *
-from .neighborlist import *
-from .manybody_neighborlist import *
-from .cluster import *
-from .permutation_map import *
-from .cluster_counts import *
-from .clusterspace import *
-from .orbit_list import *
+__description__ = 'The pythonic approach to cluster expansions'
+__authors__ = ['Mattias Ångqvist',
+               'William Armando Muñoz',
+               'Thomas Holm Rod',
+               'Paul Erhart']
+__copyright__ = ''
+__license__ = ''
+__credits__ = ['Mattias Ångqvist',
+               'William Armando Muñoz',
+               'Thomas Holm Rod',
+               'Paul Erhart']
+__version__ = '0.1'
+__all__ = ['ClusterSpace',
+           'ClusterExpansion',
+           'StructureContainer',
+           'Structure']
+__status__ = 'beta-version'
+__url__ = 'http://www.icet.org/'
