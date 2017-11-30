7862bc0ed9bdd6d0e54a0004cce108604ff0e881
angqvist <angqvist@chalmers.se>
2017-11-30 14:14:09 +0100

    #55: added new functionality to icetdev/tools submodule

--- icetdev/tools/__init__.py
+++ icetdev/tools/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import print_function, division
-from _icetdev.tools import get_unit_cell_permutation
+from _icetdev.tools import get_unit_cell_permutation, get_unit_cell_sub_permutations
 
