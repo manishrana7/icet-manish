b9d3e77f9db8c06066b45a2366a7c86c958f2683
angqvist <angqvist@chalmers.se>
2017-11-30 11:04:03 +0100

    #55: added a get unitcell permutation function to C++

--- src/Symmetry.cpp
+++ src/Symmetry.cpp
@@ -28,8 +28,38 @@ bool next_cartesian_product(const std::vector<std::vector<int>> &items, std::vec
         {
             return true;
         }
-        
     }
     return false;
+}
+
+Eigen::Matrix3i getUnitcellPermutation(const Eigen::Matrix3d &inputCell, const Eigen::Matrix3d &referenceCell, double tolerance_cell)
+{
+    // obtain the (in general non-integer) transformation matrix
+    // connecting the input configuration to the reference structure
+    // L = L_p.P --> P = L_p^-1.L
+    Eigen::Matrix3d permutationMatrix = inputCell * referenceCell.inverse();
+
+    Eigen::Matrix3d roundedPermutationMatrix;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            roundedPermutationMatrix(i, j) = roundedPermutationMatrix(i, j);
+        }
+    }
+
+    Eigen::Matrix3i integerPermutationMatrix = roundedPermutationMatrix.cast<int>();
+    Eigen::Matrix3d deviationOfCells = permutationMatrix - integerPermutationMatrix.cast<double>();
+
+    if (deviationOfCells.sum() / 9 > tolerance_cell)
+    {
+        std::string errorMessage = "Failed to map input cell to reference cell\n";
+        errorMessage += "tolerance_cell: " + std::to_string(tolerance_cell) + " \n";
+        errorMessage += "Deviation: " + std::to_string(deviationOfCells.sum() / 9) + " \n";
+        errorMessage += "You can try to raise tolerance_cell\n";
+        throw std::runtime_error(errorMessage);
+    }
+
+    return integerPermutationMatrix;
 }
 }
