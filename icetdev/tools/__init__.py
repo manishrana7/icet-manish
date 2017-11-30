49a0b37d0f94fdeb660150c28c671f9ee2c30080
angqvist <angqvist@chalmers.se>
2017-11-30 14:14:09 +0100

    #55: added new functionality to icetdev/tools submodule

--- icetdev/tools/__init__.py
+++ icetdev/tools/__init__.py
@@ -2,8 +2,11 @@ from .convex_hull import ConvexHull
 from .structure_enumeration import (enumerate_structures,
                                     get_symmetry_operations)
 from .geometry import get_primitive_structure
-
+from _icetdev.tools import (get_unit_cell_permutation,
+                            get_unit_cell_sub_permutations)
 __all__ = ['ConvexHull',
            'enumerate_structures',
            'get_symmetry_operations',
-           'get_primitive_structure']
+           'get_primitive_structure',
+           'get_unit_cell_permutation',
+           'get_unit_cell_sub_permutations']
