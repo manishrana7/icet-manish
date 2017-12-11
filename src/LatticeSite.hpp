2e9ddf54cf480aa22012f5ef52613d7a14e4f7f3
Paul Erhart <erhart@chalmers.se>
2017-12-12 00:08:00 +0100

    BUG: fixing compilation errors (#60, #72, #73)

--- src/LatticeSite.hpp
+++ src/LatticeSite.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 #include <boost/functional/hash.hpp>
 #include <iostream>
 using boost::hash;
--- src/Vector3dCompare.hpp
+++ src/Vector3dCompare.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 using namespace Eigen;
 
 struct Vector3dCompare
