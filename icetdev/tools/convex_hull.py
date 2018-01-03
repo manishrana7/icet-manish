5b8d3f7f677a38978896d64b0aea4393fb19f39d
Magnus Rahm <rmagnus@chalmers.se>
2018-01-03 10:29:28 +0100

    ENH: Implemented function that removes maximum-energy convex hull points

--- icetdev/tools/convex_hull.py
+++ icetdev/tools/convex_hull.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial import ConvexHull as ConvexHullScipy
 from scipy.interpolate import griddata
+import itertools
 
 
 class ConvexHull:
@@ -59,10 +60,74 @@ class ConvexHull:
 
         self.concentrations = concentrations
         self.energies = energies
+        print(self.energies)
 
         # Remove points that are above "pure element line"
-        #self._emove_points_above_pure_elements()
+        self.remove_points_above_pure_elements()
+        print(self.energies)
 
+    def remove_points_above_pure_elements(self, tol=1e-6):
+        '''
+        Remove all points on the convex hull that correspond to maximum rather
+        than minimum energy.
+
+        Parameters
+        ----------
+        tol : float
+            Tolerance for what energy constitutes a lower one.
+        '''
+
+        # Identify the "complex concentration hull", i.e. the extremum
+        # concentrations. In the simplest case, these should simply be the
+        # pure elements.
+        if self.dimensions == 1:
+            # Then the ConvexHullScipy function doesn't work, so we just pick
+            # the indices of the lowest and highest concentrations.
+            vertices = []
+            vertices.append(np.argmin(self.concentrations))
+            vertices.append(np.argmax(self.concentrations))
+            vertices = np.array(vertices)
+        else:
+            concentration_hull = ConvexHullScipy(self.concentrations)
+            vertices = concentration_hull.vertices
+
+        # Remove all points of the convex energy hull that have an energy that
+        # is higher than what would be gotten with pure elements at the same
+        # concentration. These points are mathematically on the convex hull,
+        # but in the physically uninteresting upper part, i.e. they maximize
+        # rather than minimize energy.
+        to_delete = []
+        for i, concentration in enumerate(self.concentrations):
+            # The points on the convex concentration should always be
+            # included, so skip them.
+            if i in vertices:
+                continue
+
+            # If there are 2 or 3 vertices on the convex concentration hull,
+            # this loop will simply yield the same array of vertices If there
+            # are more vertices, we need to check the mixed energy against all
+            # combinations of three "pure elements" (concentrations on the
+            # convex concentration hull).
+            for triangle in itertools.combinations(vertices,
+                                                   min(len(vertices), 3)):
+                # Calculate energy that would be gotten with pure elements
+                # with ascribed concentration.
+                energy_pure = griddata(self.concentrations[vertices],
+                                       self.energies[vertices],
+                                       concentration,
+                                       method='linear')
+
+                # Prepare to delete if the energy was lowered. griddata gives
+                # NaN if the concentration is outside the triangle formed by
+                # the three vertices. The result of the below comparison is
+                # then False, which is what we want.
+                if energy_pure < self.energies[i] - tol:
+                    to_delete.append(i)
+
+        # Finally remove all points
+        print(to_delete)
+        self.concentrations = np.delete(self.concentrations, to_delete, 0)
+        self.energies = np.delete(self.energies, to_delete, 0)
 
     def get_convex_hull_at_concentrations(self, target_concentrations):
 
