818d90c549f538890c2947b77d26418dc5c5d1b0
Magnus Rahm <rmagnus@chalmers.se>
2018-01-04 09:13:54 +0100

    MAINT: Added ConvexHull to __all__ to suppress flake8 complaint

--- icetdev/tools/__init__.py
+++ icetdev/tools/__init__.py
@@ -3,6 +3,7 @@ from .structure_enumeration import (enumerate_structures,
                                     get_symmetry_operations)
 from .geometry import get_primitive_structure
 
-__all__ = ['enumerate_structures',
+__all__ = ['ConvexHull',
+           'enumerate_structures',
            'get_symmetry_operations',
            'get_primitive_structure']
