41099a49fe6156561c9281284d629528e40888f9
Magnus Rahm <rmagnus@chalmers.se>
2018-01-05 09:37:41 +0100

    MAINT: Added map_sites stuff to __init__

--- icetdev/tools/__init__.py
+++ icetdev/tools/__init__.py
@@ -2,8 +2,10 @@ from .convex_hull import ConvexHull
 from .structure_enumeration import (enumerate_structures,
                                     get_symmetry_operations)
 from .geometry import get_primitive_structure
+from .map_sitse import map_structure_to_reference
 
 __all__ = ['ConvexHull',
            'enumerate_structures',
            'get_symmetry_operations',
-           'get_primitive_structure']
+           'get_primitive_structure',
+           'map_structure_to_reference']
