702fc3f155c0e9429a1f6e5a0d295a5dd30d36c9
Magnus Rahm <rmagnus@chalmers.se>
2018-01-05 09:46:35 +0100

    MAINT: Fixed spelling error

--- icetdev/tools/__init__.py
+++ icetdev/tools/__init__.py
@@ -2,7 +2,7 @@ from .convex_hull import ConvexHull
 from .structure_enumeration import (enumerate_structures,
                                     get_symmetry_operations)
 from .geometry import get_primitive_structure
-from .map_sitse import map_structure_to_reference
+from .map_sites import map_structure_to_reference
 
 __all__ = ['ConvexHull',
            'enumerate_structures',
