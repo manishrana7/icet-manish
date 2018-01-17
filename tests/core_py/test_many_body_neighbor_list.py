416d365afa22bb52686274073274741dfaf43761
angqvist <angqvist@chalmers.se>
2018-01-17 10:50:26 +0100

    #109 beefed up the mbnl test (now it passes)

--- tests/core_py/test_many_body_neighbor_list.py
+++ tests/core_py/test_many_body_neighbor_list.py
@@ -1,13 +1,15 @@
 import unittest
 
 from ase.build import bulk
+from ase.neighborlist import NeighborList
 
 from icet.core_py.many_body_neighbor_list import ManyBodyNeighborList
+
 
 class TestManyBodyNeighborList(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestManyBodyNeighborList, self).__init__(*args, **kwargs)
-        
+
         self.atoms_prim = bulk("Al")
         self.atoms = bulk("Al").repeat(2)
         self.cutoffs = [5, 5]
@@ -17,6 +19,13 @@ class TestManyBodyNeighborList(unittest.TestCase):
         Setup before each test.        
         """
         self.mbnl = ManyBodyNeighborList()
+        self.neighbor_lists = []
+        for co in self.cutoffs:
+            ase_nl = NeighborList(len(self.atoms)*[co/2], skin=1e-8,
+                             bothways=True, self_interaction=False)
+            ase_nl.update(self.atoms)
+            self.neighbor_lists.append(ase_nl)
+
 
     def test_build(self):
         """
@@ -25,8 +34,17 @@ class TestManyBodyNeighborList(unittest.TestCase):
         index = 0
         self.mbnl.build(self.neighbor_lists, index, bothways=False)
 
+    def test_bothways_true(self):
+        """
+        Build the mbnl with bothways = True and
+        assert that each index in the atoms object
+        have the same number of neighbors
+        """
+        
+        mbnl_size = len(self.mbnl.build(self.neighbor_lists, 0, bothways=True))
+        for index in range(len(self.atoms)):
+            self.assertEqual(mbnl_size,len(self.mbnl.build(self.neighbor_lists, index, bothways=True)))
 
-    
 
 
 
