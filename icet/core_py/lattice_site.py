e53935a1dfeb765108547fe54f7f348e82eadf11
angqvist <angqvist@chalmers.se>
2018-01-17 13:50:51 +0100

    #116 added initial LatticeSite object

---
+++ icet/core_py/lattice_site.py
@@ -0,0 +1,76 @@
+class LatticeSite(object):
+    """
+    Representation of a lattice site.
+    This is basically a named tuple with
+    index and offset as attributes but it
+    also defines comparing LatticeSites.
+    """
+
+    def __init__(self, index, offset):
+        """
+        LatticeSite initializer.
+
+        parameters
+        ----------
+        index : int
+            Refers to an index in a lattice
+        offset : list of ints with length 3
+            Refers to an offset of the unit
+            cell vectors.
+
+        """
+        self._index = index
+        self._offset = offset
+
+    @property
+    def index(self):
+        """
+        The index refers to an index in a lattice.
+        """
+        return self._index
+
+    @property
+    def offset(self):
+        """
+        The index refers to an index in a lattice.
+        """
+        return self._offset
+
+    def __eq__(self, other):
+        """
+        Test equality of this and another LatticeSite
+
+        Will first test index, then offset element by element.
+        It will return True/False at first difference it spots.
+        If no difference is found the two objects are equal.
+        """
+        if self.index != other.index:
+            return False
+        for i in range(3):
+            if self.offset[i] != other.offset[i]:
+                return False
+        return True
+
+    def __lt__(self, other):
+        """
+        Test if this Lattice Site is less than the
+        other LatticeSite
+
+        Will first test index, then offset element by element.
+        It will return True/False at first difference it spots.
+        If no difference is found the two objects are equal
+        and False is returned.
+        """
+        if self.index != other.index:
+            return self.index < other.index
+        for i in range(3):
+            if self.offset[i] != other.offset[i]:
+                return self.offset[i] < other.offset[i]
+        return False
+
+    def __hash__(self):
+        """
+        Generate a hash based on index and offset.
+        """
+        return hash((self.index, self.offset[0],
+                     self.offset[1], self.offset[2]))
