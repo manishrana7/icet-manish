4dcf00400d8c5e80235ca52d36cb14518ccd83d9
angqvist <angqvist@chalmers.se>
2018-01-18 17:22:26 +0100

    #116 added comparison to C++ LatticeSite in unittest

--- icet/core_py/lattice_site.py
+++ icet/core_py/lattice_site.py
@@ -1,16 +1,17 @@
 from functools import total_ordering
 
+import numpy as np
 
 @total_ordering
 class LatticeSite(object):
     """
     Representation of a lattice site.
     This is basically a named tuple with
-    index and offset as attributes but it
+    index and unitcell_offset as attributes but it
     also defines comparing LatticeSites.
     """
 
-    def __init__(self, index, offset):
+    def __init__(self, index, unitcell_offset):
         """
         LatticeSite initializer.
 
@@ -18,13 +19,16 @@ class LatticeSite(object):
         ----------
         index : int
             Refers to an index in a lattice
-        offset : list of ints with length 3
-            Refers to an offset of the unit
+        unitcell_offset : list of ints with length 3
+            Refers to an unitcell_offset of the unit
             cell vectors.
 
         """
         self._index = index
-        self._offset = offset
+        if isinstance(unitcell_offset, list):
+            self._unitcell_offset = np.array(unitcell_offset)
+        else:
+            self._unitcell_offset = unitcell_offset
 
     @property
     def index(self):
@@ -34,24 +38,24 @@ class LatticeSite(object):
         return self._index
 
     @property
-    def offset(self):
+    def unitcell_offset(self):
         """
         The index refers to an index in a lattice.
         """
-        return self._offset
+        return self._unitcell_offset
 
     def __eq__(self, other):
         """
         Test equality of this and another LatticeSite
 
-        Will first test index, then offset element by element.
+        Will first test index, then unitcell_offset element by element.
         It will return True/False at first difference it spots.
         If no difference is found the two objects are equal.
         """
         if self.index != other.index:
             return False
         for i in range(3):
-            if self.offset[i] != other.offset[i]:
+            if self.unitcell_offset[i] != other.unitcell_offset[i]:
                 return False
         return True
 
@@ -60,34 +64,29 @@ class LatticeSite(object):
         Test if this Lattice Site is less than the
         other LatticeSite
 
-        Will first test index, then offset element by element.
+        Will first test index, then unitcell_offset element by element.
         It will return True/False at first difference it spots.
         If no difference is found the two objects are equal
         and False is returned.
         """
         if self.index != other.index:
             return self.index < other.index
-        for offset, offset_other in zip(self.offset, other.offset):
-            if offset != offset_other:
-                return offset < offset_other
+        for unitcell_offset, unitcell_offset_other in zip(self.unitcell_offset, other.unitcell_offset):
+            if unitcell_offset != unitcell_offset_other:
+                return unitcell_offset < unitcell_offset_other
         return False
 
     def __hash__(self):
         """
-        Generate a hash based on index and offset.
+        Generate a hash based on index and unitcell_offset.
         """
-        return hash((self.index, self.offset[0],
-                     self.offset[1], self.offset[2]))
+        return hash((self.index, self.unitcell_offset[0],
+                     self.unitcell_offset[1], self.unitcell_offset[2]))
 
     def __str__(self):
         """
-        Return  index and offset in str format
+        Return  index and unitcell_offset in str format
         """
-        nice_str = str(self.index) + " : "
-        nice_str +="[ "
-        
-        nice_str += str(self.offset[0]) + ".  "
-        nice_str += str(self.offset[1]) + ".  "
-        nice_str += str(self.offset[2]) + "."
-        nice_str +="]"
-        return nice_str
+  
+        return '{} : {}'.format(self.index, self.unitcell_offset)
+  
