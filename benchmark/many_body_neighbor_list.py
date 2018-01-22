b323fc621b2a93e45039266bb6c1f6d610579c9f
angqvist <angqvist@chalmers.se>
2018-01-22 15:06:01 +0100

    #109 added an additional timing to benchmark only for build part

--- benchmark/many_body_neighbor_list.py
+++ benchmark/many_body_neighbor_list.py
@@ -20,10 +20,12 @@ def build_many_body_neighbor_list_cpp(structure, order, cutoff):
         nl.build(structure)
         neighbor_lists.append(nl)
 
+    t = time.process_time()
     mbnl = ManyBodyNeighborList()
     for i in range(len(structure)):
         mbnl.build(neighbor_lists, i, False)
-
+    elapsed_time = time.process_time() - t
+    return elapsed_time
 
 def build_many_body_neighbor_list_python(atoms, order, cutoff):
     '''
@@ -41,14 +43,16 @@ def build_many_body_neighbor_list_python(atoms, order, cutoff):
         neighbor_lists.append(ase_nl)
 
     bothways = False
+    t = time.process_time()
     for i in range(len(atoms)):
         mbnl_T.build(neighbor_lists, i, bothways=bothways)
-
+    elapsed_time = time.process_time() - t
+    return elapsed_time
 
 if __name__ == "__main__":
 
     order = 3
-    cutoff = 10
+    cutoff = 10.0
     atoms = bulk('Al').repeat(2)
     structure = Structure.from_atoms(atoms)
     print('Cutoff: {:.3f}'.format(cutoff))
@@ -56,12 +60,13 @@ if __name__ == "__main__":
     print('Number of atoms: {}'.format(len(atoms)))
 
     t = time.process_time()
-    build_many_body_neighbor_list_cpp(structure, order, cutoff)
+    mbnl_time_cpp = build_many_body_neighbor_list_cpp(structure, order, cutoff)
     elapsed_time_cpp = time.process_time() - t
     print('Timing C++: {:.6f} sec'.format(elapsed_time_cpp))
 
     t = time.process_time()
-    build_many_body_neighbor_list_python(atoms, order, cutoff)
+    mbnl_time_py = build_many_body_neighbor_list_python(atoms, order, cutoff)
     elapsed_time_python = time.process_time() - t
-    print('Timing Pyhton (ASE): {:.6f} s'.format(elapsed_time_python))
+    print('Timing Python (ASE): {:.6f} s'.format(elapsed_time_python))
     print('C++ speedup: {:.3f}'.format(elapsed_time_python / elapsed_time_cpp))
+    print('C++ speedup (only mbnl build): {:.3f}'.format(mbnl_time_py / mbnl_time_cpp))
