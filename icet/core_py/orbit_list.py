0b2bdbdb4838cc5cc3f5f44d4f7b7b485138f53a
angqvist <angqvist@chalmers.se>
2018-01-23 10:52:06 +0100

    #105 copied cluster_space and orbit list to core_py

---
+++ icet/core_py/orbit_list.py
@@ -0,0 +1,13 @@
+
+class OrbitList(object):
+
+    def __init__(self, atoms, cutoffs, verbosity=False):
+        pass
+
+    def sort(self):
+        """
+        Sort the list of orbits.
+        """
+        pass
+    def get_primitive_structure(self):
+        pass
