2a70af722b2418a4c8005c17feb4ad643990310e
angqvist <angqvist@chalmers.se>
2018-01-25 13:11:46 +0100

    #106 added an emppty orbit module with a TODO

---
+++ icet/core_py/orbit.py
@@ -0,0 +1,34 @@
+
+
+class Orbit(object):
+    """
+    Class Orbit
+
+    contains a list of equivalent list of LatticeSites
+    contains a sorted Cluster for representation
+    Can be compared to other orbits.
+
+    TODO
+    ----
+    * write constructor
+    * Add functions
+        * Add equivalent sites
+        * __len__
+        * geometrical_size()        
+        * getPermutatedEquivalentSites
+        * getSitesWithPermutation        
+        * sort
+        * __eq__
+        * __lt__
+        * __hash__ ?
+        * orbit + offset
+        * orbit + orbit
+        * get_mc_vectors
+    * properties 
+       * Equivalent sites + setter
+       * representative cluster
+       * Representative sites
+       * order       
+    """
+    def __init__(self):
+        pass
