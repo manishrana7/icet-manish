defe1139a4a0fc26b1ad1d139793e7c5190124b2
angqvist <angqvist@chalmers.se>
2018-01-29 13:58:23 +0100

    #106 added a top level doc string to profile script

--- benchmark/profile_orbit.py
+++ benchmark/profile_orbit.py
@@ -2,7 +2,11 @@ from benchmark.benchmark_orbit import init_python_orbit
 
 from benchmark.benchmark_orbit import time_orbit_translating
 from benchmark.benchmark_orbit import time_orbit_sites_permutations
+'''
+This script is intendet to be run in a profiler, e.g running with:
 
+python3 -m cProfile benchmark/profile_orbit.py
+'''
 
 
 
