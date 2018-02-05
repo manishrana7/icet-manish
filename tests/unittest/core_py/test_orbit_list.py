e068c6c118ab1fdf875066f0d551e3a449d31e81
angqvist <angqvist@chalmers.se>
2018-02-05 13:52:47 +0100

    #107 added test is new orbit to orbitlist

--- tests/unittest/core_py/test_orbit_list.py
+++ tests/unittest/core_py/test_orbit_list.py
@@ -17,16 +17,15 @@ class TestOrbitList(unittest.TestCase):
     Container for tests of the class functionality
     '''
 
-    def __init__(self, *args, **kwargs):        
+    def __init__(self, *args, **kwargs):
         super(TestOrbitList, self).__init__(*args, **kwargs)
         self.cutoffs = [4.2]
-        self.atoms = bulk('Ag', a=4.09)        
+        self.atoms = bulk('Ag', a=4.09)
 
     def setUp(self):
         '''
         Instantiate class before each test.
         '''
-        print("pbc in setUp ",self.atoms.pbc)
         self.orbit_list = OrbitList(self.atoms, self.cutoffs)
         # self._structure = Structure.from_atoms(self.atoms)
 
@@ -70,8 +69,16 @@ class TestOrbitList(unittest.TestCase):
     def test_is_new_orbit(self):
         """
         Test is new orbit method
+        Test this method by going through
+        all sites in the orbits and testing
+        that there are no new orbits
+        that could be made from these sites,
+        i.e. they are already taken.
         """
-        pass
+        for orbit in self.orbit_list.orbits:
+            for sites in orbit.equivalent_sites:
+                self.assertFalse(self.orbit_list.is_new_orbit(sites))
+        
 
     def test_make_orbit(self):
         """
@@ -156,6 +163,7 @@ class TestOrbitList(unittest.TestCase):
     def test_str(self):
         print(self.orbit_list)
         print(self.cluster_space_cpp)
+
         for orbit in self.orbit_list.orbits:
             for sites in sorted(orbit.equivalent_sites):
                 for site in sorted(sites):
@@ -170,7 +178,6 @@ class TestOrbitList(unittest.TestCase):
                 print()
             print("----")
 
-
     def test_singlets_particle(self):
         """
         Test that a particle get correct number of singlets.
@@ -179,24 +186,72 @@ class TestOrbitList(unittest.TestCase):
         #  one next to the corner along the side, O,
         #  one site on the surface, K,
         #  and one of the center 2x2x2 sites J
-        #  and in the darkness to bind them all 
+        #  and in the darkness to bind them all
         #
         #    from the side:
         #    X O O X
         #    H K K H
         #    H K K H
-        #    X O O X 
+        #    X O O X
         #
         #  below the K sites are another J site in the 2x2x2 positions
         #
-        atoms = bulk("Al",'sc', a=1).repeat([4,4,4])
+        atoms = bulk("Al", 'sc', a=1).repeat(4)
         atoms.pbc = False
 
-        orbit_list = OrbitList(atoms,[0])
-        cluster_space = ClusterSpace(atoms,[0],["Al","W"])
-        print(cluster_space)
+        orbit_list = OrbitList(atoms, [0])
+        cluster_space = ClusterSpace(atoms, [0], ["Al", "W"])
         self.assertEqual(len(orbit_list), 4)
-        
+
+        # Size 3 also has 4 unique sites
+        atoms = bulk("Al", 'sc', a=1).repeat(3)
+        atoms.pbc = False
+
+        orbit_list = OrbitList(atoms, [0])
+        cluster_space = ClusterSpace(atoms, [0], ["Al", "W"])
+        self.assertEqual(len(orbit_list), 4)
+
+        # Test partial pbc:
+        # Now we only distinguish sites by layers from
+        # The surface. We have 4 layers but with mirror
+        # symmetry we get 2 unique singlet orbits
+
+        atoms = bulk("Al", 'sc', a=1).repeat(4)
+        atoms.pbc = [True, True, False]
+
+        orbit_list = OrbitList(atoms, [0])
+        cluster_space = ClusterSpace(atoms, [0], ["Al", "W"])
+        self.assertEqual(len(orbit_list), 2)
+
+        # Pbc in only one direction, so we effectively have
+        # a rod-particle the unique sites are only at the surface
+        # so 1 corner, one side and one bulk
+        atoms = bulk("Al", 'sc', a=1).repeat(4)
+        atoms.pbc = [True, False, False]
+
+        orbit_list = OrbitList(atoms, [0])
+        cluster_space = ClusterSpace(atoms, [0], ["Al", "W"])
+        self.assertEqual(len(orbit_list), 3)
+
+        # Increasing size to 5 will give
+        #  one corner
+        # two on the side
+        # the center will be 3x3 so 3 sites from that
+        atoms = bulk("Al", 'sc', a=1).repeat(5)
+        atoms.pbc = [True, False, False]
+
+        orbit_list = OrbitList(atoms, [0])
+        cluster_space = ClusterSpace(atoms, [0], ["Al", "W"])
+        self.assertEqual(len(orbit_list), 6)
+
+        # Test pbc = True
+        atoms = bulk("Al", 'sc', a=1).repeat(3)
+        atoms.pbc = True
+
+        orbit_list = OrbitList(atoms, [0])
+        cluster_space = ClusterSpace(atoms, [0], ["Al", "W"])
+        self.assertEqual(len(orbit_list), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
