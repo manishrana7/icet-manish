5fac889a2fe1237f4bd6298ef2cfbbfca8584a3c
angqvist <angqvist@chalmers.se>
2018-02-13 14:15:33 +0100

    #147 begun on unit testing orbit list

--- tests/unittest/core/test_orbit_list.py
+++ tests/unittest/core/test_orbit_list.py
@@ -1,17 +1,32 @@
 import unittest
-
+from ase.build import bulk
+from icet.core.orbit_list import create_orbit_list
+from icet.core.cluster import Cluster
+from icet import Structure
 
 class TestOrbitList(unittest.TestCase):
-    '''
-    Container for test of the module functionality.
-    '''
+    """
+    Test class for testing orbit list
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(TestOrbitList, self).__init__(*args, **kwargs)
+        self.atoms = bulk("Al")
+        self.structure = Structure.from_atoms(self.atoms)
+        self.cutoffs = [10,10]
 
     def setUp(self):
-        '''
-        SetUp
-        '''
-        pass
+        """
+        Setup test.
+        """
+        self.orbit_list = create_orbit_list(self.structure, self.cutoffs)
 
+    def test_equivalent_sites_size(self):
+        """
+        Test that all the equivalent sites have the same geometrical size
+        """
 
+        for orbit in self.orbit_list.orbits:
+            orbit.representative_cluster.geometrical_size
 if __name__ == '__main__':
     unittest.main()
