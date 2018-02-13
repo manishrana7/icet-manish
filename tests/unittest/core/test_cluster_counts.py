02eab744dc5af78617322ee8ccab6c642dfdc94a
williamz <william.munoz@chalmers.se>
2018-02-13 20:26:48 +0100

    WIP: re-designing tests cases to cover ClusterCount methods

--- tests/unittest/core/test_cluster_counts.py
+++ tests/unittest/core/test_cluster_counts.py
@@ -6,24 +6,27 @@ from ase.build import bulk
 from icet.core.cluster_counts import ClusterCounts
 from icet.core.neighbor_list import get_neighbor_lists
 from icet.core.orbit_list import OrbitList
+from icet.core.many_body_neighbor_list import ManyBodyNeighborList
 from icet import Structure
 
 
 class TestClusterCounts(unittest.TestCase):
-    '''
+    """
     Container for test of the module functionality.
-    '''
+
+    WIP: Uncompleted tests.
+    """
 
     def __init__(self, *args, **kwargs):
         super(TestClusterCounts, self).__init__(*args, **kwargs)
 
-        self.cutoffs = [1.4, 0.5, 0.8]
-        self.atoms_prim = bulk('Ni', 'hcp', a=0.625, c=1.0)
-        self.neighbor_list_prim = get_neighbor_lists(
-            atoms=self.atoms_prim, cutoffs=self.cutoffs)
-        self.structure_prim = Structure.from_atoms(self.atoms_prim)
-        self.orbit_list = OrbitList(self.neighbor_list_prim,
-                                    self.structure_prim)
+        self.atoms = bulk('Ni', 'hcp', a=0.625, c=1.0).repeat(2)
+        self.neighbor_lists = get_neighbor_lists(
+            atoms=self.atoms, cutoffs=[1.4, 0.5, 0.8])
+        # orbit list
+        self.structure = Structure.from_atoms(self.atoms)
+        self.orbit_list = OrbitList(self.neighbor_lists,
+                                    self.structure)
         self.orbit_list.sort()
         self.order = []
         self.size = []
@@ -36,58 +39,71 @@ class TestClusterCounts(unittest.TestCase):
             self.multiplicity.append(len(orbit))
 
     def setUp(self):
-        '''
-        SetUp.
-        '''
+        """
+        SetUp an empty cluster counts .
+        """
         self.cluster_counts = ClusterCounts()
-        self.cluster_counts.count_clusters(self.structure_prim,
+
+    def test_count_lattice_neighbor(self):
+        """
+        Test lattice neighbors.
+        """
+        mbnl = ManyBodyNeighborList()
+        mbnl_pairs = mbnl.build(self.neighbor_lists, 0, False)
+        self.cluster_counts.count_lattice_neighbors(self.structure, mbnl_pairs)
+        cluster_map = self.cluster_counts.get_cluster_counts()
+
+        self.assertEqual(len(cluster_map), len(self.orbit_list))
+
+    def test_count(self):
+        """
+        Test count cluster.
+        """
+        pass
+
+    def test_count_orbitlist(self):
+        """
+        Test count cluster.
+        """
+        self.cluster_counts.count_clusters(self.structure,
                                            self.orbit_list, False)
-        self.cluster_map = self.cluster_counts.get_cluster_counts()
+        cluster_map = self.cluster_counts.get_cluster_counts()
 
-    def test_len_cluster_map(self):
-        '''
-        Test size of cluster map
-        '''
-        self.assertEqual(len(self.cluster_map), len(self.orbit_list))
-
-    def test_clusters_order(self):
-        '''
-        Test clusters orders
-        '''
-        for i, cluster in enumerate(sorted(self.cluster_map.keys())):
-            self.assertEqual(cluster.order, self.order[i])
-
-    def test_clusters_size(self):
-        '''
-        Test clusters size
-        '''
-        for i, cluster in enumerate(sorted(self.cluster_map.keys())):
-            self.assertEqual(cluster.geometrical_size, self.size[i])
+        self.assertEqual(len(cluster_map), len(self.orbit_list))
 
     def test_clusters_count(self):
-        '''
-        Test clusters counts
-        '''
-        for i, key in enumerate(sorted(self.cluster_map.keys())):
-            val = self.cluster_map[key]
-            for count in val.values():
+        """
+        Test multiplicity, order and size of clusters retrieved
+        from clusters count.
+        """
+        self.cluster_counts.count_clusters(self.structure,
+                                           self.orbit_list, False)
+        cluster_map = self.cluster_counts.get_cluster_counts()
+
+        for i, cluster in enumerate(sorted(cluster_map.keys())):
+            self.assertEqual(cluster.order, self.order[i])
+            self.assertEqual(cluster.geometrical_size, self.size[i])
+            counts = cluster_map[cluster]
+            for count in counts.values():
                 self.assertEqual(count, self.multiplicity[i])
 
     def test_clusters_count_low_symmetry(self):
-        '''
+        """
         Test cluster counts is larger for a
         structure with lower symmetry
-        TODO: Maybe not an optimal test
-        '''
-        atoms = self.atoms_prim.copy()
-        atoms.set_chemical_symbols(['Ni', 'Fe'])
+        """
+        atoms = self.atoms.copy()
+        # atoms.set_chemical_symbols(['Ni', 'Fe'])
         structure = Structure.from_atoms(atoms)
-        self.cluster_counts.count_clusters(structure, self.orbit_list, False)
+        cluster_counts = ClusterCounts()
+        cluster_map_low_symm = cluster_counts.get_cluster_counts()
+
+        cluster_counts.count_clusters(structure, self.orbit_list, False)
         cluster_map = self.cluster_counts.get_cluster_counts()
-        counts_low_symmetry = [list(val.values())
-                               for val in cluster_map.values()]
-        counts = [list(val.values()) for val in self.cluster_map.values()]
-        self.assertNotEqual(counts, counts_low_symmetry)
+        counts_low_symm = [list(val.values())
+                           for val in cluster_map_low_symm.values()]
+        counts = [list(val.values()) for val in cluster_map.values()]
+        self.assertEqual(counts, counts_low_symm)
 
 
 if __name__ == '__main__':
