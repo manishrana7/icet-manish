cbaeac8a7265ab326532c73c51cd1de87c58eaea
williamz <william.munoz@chalmers.se>
2018-03-20 14:17:00 +0100

    ENH/TST: tests added/changed for pairs/high-order neighbors and cubic/non-pbc cells

--- tests/unittest/core/test_many_body_neighbor_list.py
+++ tests/unittest/core/test_many_body_neighbor_list.py
@@ -3,7 +3,7 @@ import unittest
 from ase.build import bulk
 from icet import Structure
 from icet.core.lattice_site import LatticeSite
-from icet.core.neighbor_list import NeighborList
+from icet.core.neighbor_list import get_neighbor_lists
 from icet.core.many_body_neighbor_list import (
     ManyBodyNeighborList)
 
@@ -11,13 +11,13 @@ from icet.core.many_body_neighbor_list import (
 class TestManyBodyNeighborList(unittest.TestCase):
     """
     Container for test of the module functionality.
+
     """
 
     def __init__(self, *args, **kwargs):
         super(TestManyBodyNeighborList, self).__init__(*args, **kwargs)
 
-        self.atoms = bulk('Al').repeat(2)
-        # self.atoms = bulk('Ni', 'hcp', a=3.0).repeat([2, 2, 1])
+        self.atoms = bulk('Ni', 'hcp', a=3.0).repeat([2, 2, 1])
         self.cutoffs = [5.0, 5.0]
 
     def setUp(self):
@@ -25,13 +25,8 @@ class TestManyBodyNeighborList(unittest.TestCase):
         SetUp
         """
         self.mbnl = ManyBodyNeighborList()
-
-        self.neighbor_lists = []
-        for cutoff in self.cutoffs:
-            structure = Structure.from_atoms(self.atoms)
-            nl = NeighborList(cutoff)
-            nl.build(structure)
-            self.neighbor_lists.append(nl)
+        structure = Structure.from_atoms(self.atoms)
+        self.neighbor_lists = get_neighbor_lists(structure, self.cutoffs)
 
     def test_build(self):
         """
@@ -64,22 +59,44 @@ class TestManyBodyNeighborList(unittest.TestCase):
 
     def test_singlets(self):
         """
-        Test singlet.
+        Test that every singlet lattice site is listed
+        in the many-body neighbor list.
         """
         for index in range(len(self.atoms)):
             target = tuple(([LatticeSite(index, [0., 0., 0.])], []))
-            neighbors = self.mbnl.build(self.neighbor_lists, index, False)
-            self.assertEqual(neighbors[0], target)
+            singlet = self.mbnl.build(self.neighbor_lists, index, False)[0]
+            self.assertEqual(singlet, target)
 
     def test_pairs(self):
         """
-        Test pairs.
+        Test that many-body_neighbor list includes
+        all the pairs returned by  neighbor_list for
+        a specific lattice site.
         """
         index = 0
         nl_neighbors = self.neighbor_lists[0].get_neighbors(0)
         target = tuple(([LatticeSite(index, [0., 0., 0.])], nl_neighbors))
-        neighbors = self.mbnl.build(self.neighbor_lists, index, False)
-        self.assertEqual(neighbors[1], target)
+        pairs = self.mbnl.build(self.neighbor_lists, index, True)[1]
+        self.assertEqual(pairs, target)
+
+    def test_higher_order_neighbors(self):
+        """
+        Test higher order neighbors in many-body
+        neighbor list for a specific lattice site.
+        """
+        index = 0
+        high_order_neighbors = \
+            self.mbnl.build(self.neighbor_lists, index, False)[2]
+
+        target = ([LatticeSite(0, [0, 0, 0]), LatticeSite(0, [0, 0, 1])],
+                  [LatticeSite(1, [0, 0, 0]),
+                   LatticeSite(3, [0, -1, 0]),
+                   LatticeSite(5, [-1, -1, 0]),
+                   LatticeSite(5, [-1, 0, 0]),
+                   LatticeSite(5, [0, 0, 0]),
+                   LatticeSite(7, [-1, -1, 0])])
+
+        self.assertEqual(target, high_order_neighbors)
 
     def test_calculate_intersections(self):
         """
@@ -103,34 +120,59 @@ class TestManyBodyNeighborList(unittest.TestCase):
 
     def test_mbnl_non_pbc(self):
         """
-        Test many-body neighbor list for non-pbc structure gives
-        less set of lattice neighbors under the same cutoffs.
+        Test many-body neighbor list for non-pbc structure.
         """
         atoms = self.atoms.copy()
         atoms.set_pbc([False])
-        neighbor_lists = []
-        for cutoff in self.cutoffs:
-            structure = Structure.from_atoms(atoms)
-            nl = NeighborList(cutoff)
-            nl.build(structure)
-            neighbor_lists.append(nl)
+        structure = Structure.from_atoms(atoms)
+        neighbor_lists = get_neighbor_lists(structure, self.cutoffs)
 
         mbnl = ManyBodyNeighborList()
-        lattice_neighbors_non_pbc = []
-        lattice_neighbors = []
-        # add the mbnl lattice neighbors
-        for lattice_index in range(len(structure)):
-            neighbors_non_pbc = mbnl.build(neighbor_lists,
-                                           lattice_index, False)
-            neighbors = mbnl.build(self.neighbor_lists,
-                                   lattice_index, False)
-            for lat_nbrs in neighbors_non_pbc:
-                lattice_neighbors_non_pbc.append(lat_nbrs)
-            for lat_nbrs in neighbors:
-                lattice_neighbors.append(lat_nbrs)
 
-        self.assertLess(len(lattice_neighbors_non_pbc),
-                        len(lattice_neighbors))
+        target = [([LatticeSite(0, [0, 0, 0])], []),
+                  ([LatticeSite(0, [0, 0, 0])],
+                   [LatticeSite(1, [0, 0, 0]),
+                    LatticeSite(2, [0, 0, 0]),
+                    LatticeSite(4, [0, 0, 0]),
+                    LatticeSite(5, [0, 0, 0]),
+                    LatticeSite(6, [0, 0, 0])]),
+                  ([LatticeSite(0, [0, 0, 0]), LatticeSite(1, [0, 0, 0])],
+                   [LatticeSite(2, [0, 0, 0]), LatticeSite(4, [0, 0, 0]),
+                    LatticeSite(5, [0, 0, 0]), LatticeSite(6, [0, 0, 0])]),
+                  ([LatticeSite(0, [0, 0, 0]), LatticeSite(2, [0, 0, 0])],
+                   [LatticeSite(6, [0, 0, 0])]),
+                  ([LatticeSite(0, [0, 0, 0]), LatticeSite(4, [0, 0, 0])],
+                   [LatticeSite(5, [0, 0, 0]), LatticeSite(6, [0, 0, 0])]),
+                  ([LatticeSite(0, [0, 0, 0]), LatticeSite(5, [0, 0, 0])],
+                   [LatticeSite(6, [0, 0, 0])])]
+
+        neighbors_non_pbc = mbnl.build(neighbor_lists, 0, False)
+
+        for k, latt_neighbors in enumerate(neighbors_non_pbc):
+            self.assertEqual(target[k], latt_neighbors)
+
+    def test_mbnl_cubic_non_pbc(self):
+        """
+        Test that corners sites in a large cubic cell have
+        only three neighbors in many-body neighbor list.
+        """
+        atoms = bulk('Al', 'sc', a=4.0).repeat(4)
+        atoms.set_pbc(False)
+
+        structure = Structure.from_atoms(atoms)
+        neighbor_lists = get_neighbor_lists(structure, self.cutoffs)
+
+        mbnl = ManyBodyNeighborList()
+        # atomic indices located at the corner of structure
+        corner_sites = [0, 3, 12, 15, 48, 51, 60, 63]
+        for index in corner_sites:
+            lattice_neighbor = mbnl.build(neighbor_lists,
+                                          index, True)
+            # check pairs
+            self.assertEqual(len(lattice_neighbor[1][1]), 3)
+            # not neighbors besides above pairs
+            with self.assertRaises(IndexError):
+                lattice_neighbor[2]
 
 
 if __name__ == '__main__':
