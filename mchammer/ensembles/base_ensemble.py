dc6563618264c0eb0513b546cd3da9a295c82344
angqvist <angqvist@chalmers.se>
2018-03-20 11:27:44 +0100

    #162 added methods to base ensemble and its unittest

--- mchammer/ensembles/base_ensemble.py
+++ mchammer/ensembles/base_ensemble.py
@@ -1,8 +1,133 @@
-'''
-Definition of the base ensemble class.
-'''
+from abc import ABC, abstractmethod
+from mchammer.calculator import Calculator
 
-class BaseEnsemble:
 
-    def __init__(self):
+class BaseEnsemble(ABC):
+    """
+    Base ensemble abstract class.
+
+    Attributes
+    ----------
+    name : str
+        Name of the ensemble.
+    accepted_trials : int
+        Number of accepted trial steps.
+    total_trials : int
+        Numer of total trial steps.
+    """
+
+    def __init__(self, atoms, cluster_expansion, name='BaseEnsemble',
+                 random_seed=42):
+
+        self._cluster_expansion = cluster_expansion
+        self._calculator = Calculator(atoms, self.cluster_expansion)
+        self._atoms = atoms
+        self._name = name
+        self.accepted_trials = 0
+        self.total_trials = 0
+        self._random_seed = random_seed
+        self._boltzmann_constant = 8.6173303e-5
+        self._observers = {}
+
+    @property
+    def structure(self):
+        """
+        ASE Atoms object :
+        The current state of the  structure being sampled in the ensemble.
+        """
+        return self._atoms
+
+    @property
+    def observers(self):
+        """
+        dict of mchammer Observer's.
+        """
+        return self._observers
+
+    @property
+    def acceptance_ratio(self):
+        """
+        Returns the acceptance ratio,
+        i.e. accepted_trials / total_trials.
+        """
+        return self.accepted_trials / self.total_trials
+
+    @property
+    def cluster_expansion(self):
+        """
+        icet ClusterExpansion.
+        """
+        return self._cluster_expansion
+
+    @property
+    def calculator(self):
+        """
+        mchammer Calculator.
+        """
+        return self._calculator
+
+    def run(self, trial_moves, observation_interval=None):
+        """
+        Sample the ensemble `trial_moves` steps.
+
+        Parameters:
+        -----------
+
+        trial_moves: int
+            number of steps to run in total
+        """
         pass
+
+    @abstractmethod
+    def do_trial_move(self):
+        pass
+
+    @property
+    def name(self):
+        """
+        str : Name of BaseEnsemble.
+        """
+        return self._name
+
+    @property
+    def random_seed(self):
+        """
+        int : Random seed used in random number generator.
+        """
+        return self._random_seed
+
+    def next_random_number(self):
+        """
+        Return the next random number from the RNG.
+        """
+        pass
+
+    @property
+    def kB(self):
+        """
+        Boltzmann constant in eV / K.
+        """
+        return self._boltzmann_constant
+
+    @abstractmethod
+    def _run(self, trial_moves):
+        """
+        Internal running of the MC without interruption.
+
+        trial_moves : int
+           Number of trial moves to run without stopping.
+        """
+        pass
+
+    def attach_observer(self, observer, tag=None):
+        """
+        Attaches an observer to the ensemble.
+
+        Parameters:
+        ----------
+        observer : mchammer Observer object
+        """
+        if tag is not None:
+            self.observers[tag] = observer
+        else:
+            self.observers[observer.tag] = observer
---
+++ tests/unittest/mchammer/test_base_ensemble.py
@@ -0,0 +1,112 @@
+
+import unittest
+from icet import ClusterSpace
+from icet import ClusterExpansion
+from mchammer.ensembles.base_ensemble import BaseEnsemble
+from ase.build import bulk
+import numpy as np
+
+
+class TestEnsemble(unittest.TestCase):
+    """
+    Container for tests of the class functionality
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(TestEnsemble, self).__init__(*args, **kwargs)
+
+        self.atoms = bulk("Al")
+        cutoffs = [5, 5, 4]
+        elements = ["Al", "Ga"]
+        self.cs = ClusterSpace(self.atoms, cutoffs, elements)
+        parameters = np.array([1.2 for _ in range(len(self.cs))])
+        self.ce = ClusterExpansion(self.cs, parameters)
+
+    def setUp(self):
+        """
+        Setup before each test.
+        """
+        # Create a concreate child of Ensemble for testing
+        class ConcreteEnsemble(BaseEnsemble):
+
+            def __init__(self, atoms, cluster_expansion, name):
+                super().__init__(atoms, cluster_expansion, name)
+
+            def do_trial_move(self):
+                pass
+
+            def _run(self):
+                pass
+        self.ensemble = ConcreteEnsemble(self.atoms, self.ce, 'test-ensemble')
+
+    def test_property_name(self):
+        """
+        Test name property.
+        """
+
+        self.assertEqual('test-ensemble', self.ensemble.name)
+
+    def test_property_random_seed(self):
+        """
+        Test random seed property.
+        """
+
+        self.assertEqual(self.ensemble.random_seed, 42)
+
+    def test_property_accepted_trials(self):
+        """
+        Test property accepted trials.
+        """
+
+        self.assertEqual(self.ensemble.accepted_trials, 0)
+        self.ensemble.accepted_trials += 1
+        self.assertEqual(self.ensemble.accepted_trials, 1)
+
+    def test_property_totals_trials(self):
+        """
+        Test property accepted trials.
+        """
+
+        self.assertEqual(self.ensemble.total_trials, 0)
+        self.ensemble.total_trials += 1
+        self.assertEqual(self.ensemble.total_trials, 1)
+
+    def test_test_boltzmann_constant(self):
+        """
+        Test boltzmann constant.
+        """
+        self.assertEqual(self.ensemble.kB, 8.6173303e-5)
+
+    def test_property_cluster_expansion(self):
+        """
+        Test the cluster expansion property.
+        """
+        self.assertEqual(self.ensemble.cluster_expansion, self.ce)
+
+    def test_run(self):
+        """
+        Test the run method.
+        """
+        pass
+
+    def test_internal_run(self):
+        """
+        Test the _run method.
+        """
+        pass
+
+    def test_property_structure(self):
+        """
+        Test the get current structure method.
+        """
+        self.assertEqual(self.ensemble.structure, self.atoms)
+
+    def test_attach_observer(self):
+        """
+        Test the attach method.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
