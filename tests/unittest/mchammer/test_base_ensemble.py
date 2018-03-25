53d88818597c5f7b43214cc172182cb9341c148e
Paul Erhart <erhart@chalmers.se>
2018-03-25 12:50:15 +0200

    STY: pylinted mchammer base_ensemble test

--- tests/unittest/mchammer/test_base_ensemble.py
+++ tests/unittest/mchammer/test_base_ensemble.py
@@ -1,5 +1,5 @@
-
 import unittest
+
 from icet import ClusterSpace
 from icet import ClusterExpansion
 from mchammer.ensembles.base_ensemble import BaseEnsemble
@@ -8,9 +8,7 @@ import numpy as np
 
 
 class TestEnsemble(unittest.TestCase):
-    """
-    Container for tests of the class functionality
-    """
+    """Container for tests of the class functionality."""
 
     def __init__(self, *args, **kwargs):
         super(TestEnsemble, self).__init__(*args, **kwargs)
@@ -23,10 +21,8 @@ class TestEnsemble(unittest.TestCase):
         self.ce = ClusterExpansion(self.cs, parameters)
 
     def setUp(self):
-        """
-        Setup before each test.
-        """
-        # Create a concreate child of Ensemble for testing
+        """Setup before each test."""
+        # Create a concrete child of Ensemble for testing
         class ConcreteEnsemble(BaseEnsemble):
 
             def __init__(self, atoms, calculator, name=None, random_seed=None):
@@ -42,97 +38,64 @@ class TestEnsemble(unittest.TestCase):
             self.atoms, calculator=None, name='test-ensemble', random_seed=42)
 
     def test_property_name(self):
-        """
-        Test name property.
-        """
-
+        """Test name property."""
         self.assertEqual('test-ensemble', self.ensemble.name)
 
     def test_property_random_seed(self):
-        """
-        Test random seed property.
-        """
-
+        """Test random seed property."""
         self.assertEqual(self.ensemble.random_seed, 42)
 
     def test_property_accepted_trials(self):
-        """
-        Test property accepted trials.
-        """
-
+        """Test property accepted trials."""
         self.assertEqual(self.ensemble.accepted_trials, 0)
         self.ensemble.accepted_trials += 1
         self.assertEqual(self.ensemble.accepted_trials, 1)
 
     def test_property_totals_trials(self):
-        """
-        Test property accepted trials.
-        """
-
+        """Test property accepted trials."""
         self.assertEqual(self.ensemble.total_trials, 0)
         self.ensemble.total_trials += 1
         self.assertEqual(self.ensemble.total_trials, 1)
 
     def test_property_calculator(self):
-        """
-        Test the calculator property.
-        """
+        """Test the calculator property."""
         pass
 
     def test_get_next_random_number(self):
-        """
-        Test the get_next_random_number method.
-        """
+        """Test the get_next_random_number method."""
         self.assertAlmostEqual(
             self.ensemble.next_random_number(), 0.6394267984578837)
 
     def test_run(self):
-        """
-        Test the run method.
-        """
+        """Test the run method."""
         pass
 
     def test_internal_run(self):
-        """
-        Test the _run method.
-        """
+        """Test the _run method."""
         pass
 
     def test_property_structure(self):
-        """
-        Test the get current structure method.
-        """
+        """Test the get current structure method."""
         self.assertEqual(self.ensemble.structure, self.atoms)
 
     def test_attach_observer(self):
-        """
-        Test the attach method.
-        """
+        """Test the attach method."""
         pass
 
     def test_property_data_container(self):
-        """
-        Test the data container property.
-        """
+        """Test the data container property."""
         pass
 
     def test_find_minimum_observation_interval(self):
-        """
-        Test the method to find the minimum observation interval.
-        """
+        """Test the method to find the minimum observation interval."""
         pass
 
     def test_property_minimum_observation_interval(self):
-        """
-        Test property minimum observation interval.
-        """
+        """Test property minimum observation interval."""
         pass
 
     def test_get_gcd(self):
-        """
-        Test the get gcd method.
-        """
-
+        """Test the get gcd method."""
         input = [2, 4, 6, 8]
         target = 2
         self.assertEqual(self.ensemble._get_gcd(input), target)
