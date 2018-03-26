3c3f2c3d61526cf8141cb7e0aea89c4da90c9727
angqvist <angqvist@chalmers.se>
2018-03-26 17:14:32 +0200

    #167 added cluster expansion calculator

---
+++ mchammer/calculators/cluster_expansion_calculator.py
@@ -0,0 +1,63 @@
+from mchammer.calculators.base_calculator import BaseCalculator
+
+
+class ClusterExpansionCalculator(BaseCalculator):
+    """
+    Cluster expansion calculator.
+
+    Class for efficiently calculating the
+    cluster expansion property
+    for a specific structure
+
+    Parameters
+    ----------
+    atoms : ASE Atoms object
+        the structure which the calculator can use
+        to optimize the calculate function.
+
+    cluster_expansion : icet ClusterExpansion object
+    name : str
+        Human readable identifier for this calculator
+
+    """
+
+    def __init__(self, atoms, cluster_expansion, name='Cluster Expansion Calculator'):
+        super().__init__(atoms=atoms, name=name)
+
+        self._cluster_expansion = cluster_expansion
+
+    @property
+    def cluster_expansion(self):
+        """
+        icet cluster expansion.
+        """
+        return self._cluster_expansion
+
+    def calculate_total(self):
+        """
+        Calculator the total property of the current configuration.
+
+        Return:
+            total_property : float
+        """
+        return self.cluster_expansion.predict(self.atoms)
+
+    def calculate_local_contribution(self, indices):
+        """
+        return the local contribution of the index in indices.
+
+        Return:
+            local_contribution : float
+        """
+        local_contribution = 0
+        for index in indices:
+            local_contribution += self._calculate_local_contribution(index)
+
+        return local_contribution
+
+    def _calculate_local_contribution(self, index):
+        """
+        Internal method to calculate the local contribtuion for one
+        index.
+        """
+        return self.calculate_total()
---
+++ tests/unittest/mc/test_cluster_expansion_calculator.py
@@ -0,0 +1,85 @@
+import unittest
+
+from ase.atoms import Atoms
+from ase.build import bulk
+from icet import ClusterExpansion, ClusterSpace
+from mchammer.calculators.base_calculator import BaseCalculator
+from mchammer.calculators.cluster_expansion_calculator import \
+    ClusterExpansionCalculator
+
+
+class TestCECalculator(unittest.TestCase):
+    """
+    Container for tests of the class functionality.
+
+    TODO:
+        * add property test to calculate local contribution when that
+          method has been added as intentional.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(TestCECalculator, self).__init__(*args, **kwargs)
+
+        self.atoms = bulk("Al").repeat(3)
+        self.cutoffs = [6, 6, 5]
+        self.subelements = ['Al', 'Ge']
+        self.cs = ClusterSpace(self.atoms, self.cutoffs, self.subelements)
+        params_len = self.cs.get_cluster_space_size()
+        params = list(range(params_len))
+
+        self.ce = ClusterExpansion(self.cs, params)
+
+    def setUp(self):
+        """Setup before each test."""
+        self.calculator = ClusterExpansionCalculator(
+            self.atoms, self.ce, name='Test CE calc')
+
+    def test_property_cluster_expansion(self):
+        """
+        Test the cluster expansion property.
+        """
+        self.assertIsInstance(
+            self.calculator.cluster_expansion, ClusterExpansion)
+
+    def test_calculate_total(self):
+        """
+        Test calculating total property.
+        """
+
+        self.assertEqual(self.calculator.calculate_total(), 283.0)
+        self.assertEqual(self.calculator.cluster_expansion.predict(
+            self.calculator.atoms), 283.0)
+
+        # set some elements
+        indices = [10, 2, 4, 2]
+        elements = [32] * 4
+        self.calculator.set_elements(indices, elements)
+        self.assertAlmostEqual(self.calculator.calculate_total(), 66.96296296)
+        self.assertAlmostEqual(self.calculator.cluster_expansion.predict(
+            self.calculator.atoms),  66.96296296)
+
+    def test_calculate_local_contribution(self):
+        """
+        Test calculate local contribution.
+        """
+        indices = [1, 2, 3]
+        local_contribution = self.calculator.calculate_local_contribution(
+            indices)
+        self.assertIsInstance(local_contribution, float)
+
+    def test_internal_calc_local_contribution(self):
+        """
+        Test the internal calc local contribution.
+        """
+        indices = [1, 2, 3]
+        local_contribution = 0
+        for index in indices:
+            local_contribution += self.calculator._calculate_local_contribution(
+                index)
+        self.assertEqual(local_contribution,
+                         self.calculator.calculate_local_contribution(indices))
+
+
+if __name__ == '__main__':
+    unittest.main()
