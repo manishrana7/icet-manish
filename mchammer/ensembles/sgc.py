607532cbd193e22deb3144b94c48ceed271e7ae9
Paul Erhart <erhart@chalmers.se>
2018-03-28 00:09:47 +0200

    MAINT: clean up of incomplete ensembles and observer classes

--- mchammer/ensembles/sgc.py
+++
@@ -1,9 +0,0 @@
-"""Definition of the semi-grand canonical ensemble class."""
-
-from mchammer.ensembles.base_ensemble import BaseEnsemble
-
-
-class SGC(BaseEnsemble):
-
-    def __init__(self):
-        pass
