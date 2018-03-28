ae8b777266a100465934067983717ad80d9dcfd7
williamz <william.munoz@chalmers.se>
2018-03-28 16:34:35 +0200

    ENH: append function modified, docstring cleaned and some typos fixed

--- mchammer/data_container.py
+++ mchammer/data_container.py
@@ -1,81 +1,77 @@
 import pandas as pd
 from datetime import date as dt
-
+from datetime import datetime
+import getpass
+import socket
 from ase import Atoms
 from collections import OrderedDict
+from icet.io.logging import logger
 
 
-class DataContainer(object):
+logger = logger.getChild('data_container')
+
+
+class DataContainer:
     """
-    Data Container class which serves to store
-    all information concerned to Monte Carlo
-    calculation performed with mchammer.
+    Data container class, which serves for storing
+    all information concerned with Monte Carlo
+    simulations performed with mchammer.
 
     Todo
     ----
-    * Append function needs improvements
-
+    * append function needs improving
     """
 
-    def __init__(self, base_ensemble):
+    def __init__(self, ensemble):
         """
         Initialize a DataContainer object.
 
-        Attributes:
+        Attributes
         ----------
-        base_ensemble : BaseEnsemble object
-
+        ensemble : BaseEnsemble object
         """
-        self._base_ensemble = base_ensemble
 
         self._observables = OrderedDict()
         self._parameters = OrderedDict()
         self._metadata = OrderedDict()
-
-        self._row_data = OrderedDict()
         self._data = pd.DataFrame()
+
+        # self._add_metadata('ensemble-name', ensemble.name)
+        self._add_metadata('date-created', datetime.now())
+        self._add_metadata('username', getpass.getuser())
+        self._add_metadata('hostname', socket.gethostname())
 
     def add_structure(self, atoms):
         """
-        Add a reference atomic structure to
-        data container.
+        Add a reference atomic structure to the data container.
 
-        Parameters :
+        Parameters
         ----------
         atoms : ASE Atoms object
-
         """
+        assert isinstance(atoms, Atoms), \
+            'Structure must be provided as ASE Atoms object'
         atoms_copy = atoms.copy()
-
-        msg = 'Structure has not ASE atoms type'
-        assert isinstance(atoms_copy, Atoms), msg
-
         self.structure = atoms_copy
 
-    def add_observable(self, tag, property_type):
+    def add_observable(self, tag: str, obs_type):
         """
-        Add observable to list of observables
-        which serves as headers of the data frame
-        containing the runtime data.
+        Add an observable to the list of observables.
 
-        Parameters :
+        Parameters
         ----------
         tag : str
-            tag used to name observable
-
+            name of observable used as header for the pandas data frame
         property_type : type
             type of observable parameter
-
         """
-        msg = 'Observable tag has wrong type (str)'
-        assert isinstance(tag, str), msg
+        assert isinstance(tag, str), \
+            'Observable tag has wrong type (str)'
+        assert isinstance(obs_type, (type, list)), \
+            'Unknown observable type'
+        self._observables[tag] = obs_type
 
-        msg = 'Unknow observable type'
-        assert isinstance(property_type, (type, list)), msg
-
-        self._observables[tag] = property_type
-
-    def add_parameter(self, tag, property_value):
+    def add_parameter(self, tag, value):
         """
         Add parameter required for initial setting
         of the associated ensemble.
@@ -84,171 +80,141 @@ class DataContainer(object):
         ----------
         tag : str
             name of the parameter
-
-        property_value : int, float
-            value of the parameter
-
+        value : int, float, list of int or float
+            parameter value
         """
-        msg = 'Parameter tag has wrong type (str)'
-        assert isinstance(tag, str), msg
+        assert isinstance(tag, str), \
+            'Parameter tag has the wrong type (str).'
+        assert isinstance(value, (int, float, list)), \
+            'Unknown parameter type: {}'.format(type(value))
+        self._parameters[tag] = value
 
-        msg = 'Unknow observable type'
-        assert isinstance(property_value, (int, float, list)), msg
-
-        self._parameters[tag] = property_value
-
-    def add_metadata(self, tag, input_value):
+    def _add_metadata(self, tag, value):
         """
-        Add descriptive metadata (author, hostname, time).
+        Add metadata to the data container.
 
-        Parameters :
+        Parameters
         ----------
         tag : str
-            name of the metadata
-
-        input_value : str, date
-            value of the input metadata
-
-        Todo
-        ----
-        * Include name of ensemble, observer, keywords
-
+            name of the metadata field
+        value : str, date, int
+            value of the metadata
         """
-        msg = 'Metadata tag has wrong type (str)'
-        assert isinstance(tag, str), msg
+        assert isinstance(tag, str), \
+            'Metadata tag has wrong type (str)'
+        assert isinstance(value, (str, dt, int)), \
+            'Unknown metadata type'
+        if tag in self._metadata:
+            logger.warning('Cannot overwrite existing metadata ({})'
+                           .format(tag))
+            return
+        self._metadata[tag] = value
 
-        msg = 'Unknow metadata type'
-        assert isinstance(input_value, (str, dt)), msg
-
-        self._metadata[tag] = input_value
-
-    def append(self, mcstep, tag, input_value):
+    def append(self, mctrial: int, record: dict):
         """
-        Append runtime data to data row dict.
+        Append runtime data to buffer dict.
 
-        Parameters :
+        Parameters
         ----------
-
-        mcstep : int
+        mctrial : int
             current Monte Carlo step
-
-        tag : str
-            name of parameter or observable
-
-        input_value: int, float, list
-            value of parameter or observable to be appended
-            to data container
+        record : dict
+            dictionary of tag-value pairs representing observations
 
         Todo
         ----
         Might be a quite expensive way to add data
         to data frame.
-
         """
-        msg = 'Monte Carlo step has wrong type (int)'
-        assert isinstance(mcstep, int), msg
+        assert isinstance(mctrial, int), \
+            'Monte Carlo step has wrong type (int)'
+        assert isinstance(record, dict), \
+            'Input record has wrong type (dict)'
 
-        msg = 'Input tag has wrong type (str)'
-        assert isinstance(tag, str), msg
+        self._row_data = OrderedDict()
+        self._row_data['mctrial'] = mctrial
+        self._row_data.update(record)
 
-        msg = 'Input value has unknown type'
-        assert isinstance(input_value, (int, float, list)), msg
-
-        self._row_data['mcstep'] = mcstep
-        self._row_data[tag] = input_value
-
-    def update(self):
-        """
-        Append current data row to data frame
-        and clear dict for the next row of data.
-
-        """
         self._data = self._data.append(self._row_data,
                                        ignore_index=True)
-        # restart row
-        self._row_data.clear()
 
-    def get_data(self, key_tags):
+    def get_data(self, tags):
         """
         Returns a list of lists with the current
-        data stored in the Data Frame.
+        data stored in the Pandas data frame.
 
-        Parameters :
+        Parameters
         ----------
-
-        keys : list of str
+        tags : list of str
             list with tags of the required properties.
-
         """
-        msg = 'observable/parameter is not listed in DataContainer'
         data_list = []
         for row in range(len(self._data)):
             data_row = []
-            for key in key_tags:
-                assert key in self._data, msg
-                data_row.append(self._data.loc[row, key])
+            for tag in tags:
+                assert tag in self._data, \
+                    'observable/parameter is not part of DataContainer'
+                data_row.append(self._data.loc[row, tag])
             data_list.append(data_row)
-
         return data_list
 
     @property
     def parameters(self):
-        """
-        Return added parameters.
-        """
-        return self._parameters
+        """list : ensemble parameters"""
+        return self._parameters.copy()
 
     @property
     def observables(self):
-        """
-        Return added observables.
-        """
+        """list of BaseObserver objects : observables"""
         return self._observables
 
     @property
     def metadata(self):
-        """
-        Return added metadata.
-        """
+        """list : metadata associated with data container"""
         return self._metadata
 
     def reset(self):
-        """
-        Restart data frame in data container.
-        """
+        """Reset data frame of data container."""
         self._data = pd.DataFrame()
 
     def __len__(self):
-        """
-        Return number of existing rows in data frame.
-        """
+        """number of rows in data frame"""
         return len(self._data)
 
-    def get_average(self):
+    def get_average(self, tag: str):
         """
         Return average over MonteCarlo steps of parameter
         and/or observable.
+
+        Parameters
+        ----------
+        tag : str
+            tag of field over which to average
         """
         pass
 
-    def load(self):
+    def read(self, infile):
         """
-        Load data container from file.
+        Read DataContainer object from file.
+
+        Parameters
+        ----------
+        infile : str or FileObj
+            file from which to read
+        """
+        pass
+
+    def write(self, outfile):
+        """
+        Write DataContainer object to file.
+
+        Parameters
+        ----------
+        outfile : str or FileObj
+            file to which to write
 
         Todo
         ----
         * To be implemented in a different issue.
-
-        """
-        pass
-
-    def write(self):
-        """
-        Write data container to file.
-
-        Todo
-        ----
-        * To be implemented in a different issue.
-
         """
         pass
--- tests/unittest/mc/test_data_container.py
+++ tests/unittest/mc/test_data_container.py
@@ -5,26 +5,26 @@ import unittest
 from mchammer import BaseEnsemble, DataContainer
 from icet import ClusterSpace, ClusterExpansion
 from ase.build import bulk
+from datetime import datetime
 from collections import OrderedDict
-from datetime import date
 
 
 class TestDataContainer(unittest.TestCase):
     """
     Container for the tests of the class functionality
-
     """
     def __init__(self, *args, **kwargs):
         super(TestDataContainer, self).__init__(*args, **kwargs)
-        subelements = ['W', 'I', 'Li']
+        subelements = ['Ag', 'Pd', 'Au']
         cutoffs = [1.4] * 3
         atoms_prim = bulk("Al")
         cluster_space = ClusterSpace(atoms_prim, cutoffs, subelements)
         params = list(range(len(cluster_space)))
 
-        self.observers = ['some_observer']
-        self.obs_tag = 'energy'
-        self.obs_observable = -12.0
+        self.observers = ['termostat', 'other_obs']
+        self.intervals = [100, 200]
+        self.obs_tag = ['temperature', 'energy']
+        self.obs_observable = [100.0, 100.0]
 
         self.ce = ClusterExpansion(cluster_space, params)
         # self.atoms = self.ce.primitive_structure.repeat(4)
@@ -32,7 +32,7 @@ class TestDataContainer(unittest.TestCase):
 
     def setUp(self):
         """
-        setUp all tests with this
+        setUp all tests with this.
         """
         SomeEnsemble = BaseEnsemble(atoms=self.atoms,
                                     calculator=self.ce)
@@ -64,35 +64,21 @@ class TestDataContainer(unittest.TestCase):
         self.data.add_parameter('chemical-potential-difference', -0.5)
         self.assertEqual(len(self.data.parameters), 2)
 
-    def test_add_metadata(self):
-        """
-        Tests that metadata is added to DataContainer.
-        """
-        today = date.today()
-        author, hostname, time = 'user', 'host', today
-
-        self.data.add_metadata('author', author)
-        self.data.add_metadata('hostname', hostname)
-        self.data.add_metadata('date', time)
-
-        self.assertEqual(len(self.data.metadata), 3)
-
     def test_append_data(self):
         """
         Test that data is appended to DataContainer.
         """
-        minimum_interval = 10
-        obs_interval = 100
-        for mcstep in range(1, 1001):
-            if mcstep % minimum_interval == 0:
-                for obs in self.observers:
-                    if mcstep % obs_interval == 0:
-                        self.data.append(mcstep,
-                                         self.obs_tag,
-                                         self.obs_observable)
-                        self.data.update()
+        for mctrial in range(1, 1001):
+            if mctrial % min(self.intervals) == 0:
+                row_data = OrderedDict()
+                for k, obs in enumerate(self.observers):
+                    if mctrial % self.intervals[k] == 0:
+                        tag = self.obs_tag[k]
+                        observable = self.obs_observable[k]
+                        row_data[tag] = observable
+                self.data.append(mctrial, row_data)
 
-        self.assertEqual(len(self.data), minimum_interval)
+        self.assertEqual(len(self.data), 10)
 
     def test_parameters(self):
         """
@@ -117,66 +103,61 @@ class TestDataContainer(unittest.TestCase):
         """
         target = OrderedDict([('energy', float),
                               ('temperature', float),
-                              ('sro', list)])
+                              ('sro1', list)])
 
         self.data.add_observable('energy', float)
         self.data.add_observable('temperature', float)
-        self.data.add_observable('sro', list)
+        self.data.add_observable('sro1', list)
         retval = self.data.observables
 
         self.assertEqual(retval, target)
 
     def test_metadata(self):
         """
-        Test added metadata.
+        Test metadata.
         """
-        target = OrderedDict([('author', 'user'),
-                              ('hostname', 'host'),
-                              ('date', date(2018, 3, 23))])
-
-        this_date = date(2018, 3, 23)
-        author, hostname, time = 'user', 'host', this_date
-
-        self.data.add_metadata('author', author)
-        self.data.add_metadata('hostname', hostname)
-        self.data.add_metadata('date', time)
-        retval = self.data.metadata
-
-        self.assertDictEqual(retval, target)
+        for key in self.data.metadata:
+            retval = self.data.metadata[key]
+            if key == 'date-created':
+                self.assertIsInstance(retval, datetime)
+            else:
+                self.assertIsInstance(retval, str)
 
     def test_get_data(self):
         """
         Test that data is returned from DataContainer
         has right format (list of lists).
         """
-        target = [[100.0, -12.0], [200.0, -12.0], [300.0, -12.0],
-                  [400.0, -12.0], [500.0, -12.0], [600.0, -12.0],
-                  [700.0, -12.0], [800.0, -12.0], [900.0, -12.0]]
+        target = [[100.0, 100.0], [200.0, 100.0], [300.0, 100.0],
+                  [400.0, 100.0], [500.0, 100.0], [600.0, 100.0],
+                  [700.0, 100.0], [800.0, 100.0], [900.0, 100.0],
+                  [1000.0, 100.0]]
 
-        minimum_interval = 10
-        obs_interval = 100
-        for mcstep in range(1, 1000):
-            if mcstep % minimum_interval == 0:
-                for obs in self.observers:
-                    if mcstep % obs_interval == 0:
-                        self.data.append(mcstep,
-                                         self.obs_tag,
-                                         self.obs_observable)
-                        self.data.update()
-        retval = self.data.get_data(['mcstep', self.obs_tag])
+        for mctrial in range(1, 1001):
+            if mctrial % min(self.intervals) == 0:
+                row_data = OrderedDict()
+                for k, obs in enumerate(self.observers):
+                    if mctrial % self.intervals[k] == 0:
+                        tag = self.obs_tag[k]
+                        observable = self.obs_observable[k]
+                        row_data[tag] = observable
+                self.data.append(mctrial, row_data)
+
+        retval = self.data.get_data(['mctrial', 'temperature'])
         self.assertListEqual(target, retval)
 
         with self.assertRaises(AssertionError):
-            self.data.get_data(['temperature'])
+            self.data.get_data(['concentration'])
 
     def test_reset(self):
         """
         Test this functionality removes the
         appended data to DataContainer.
         """
-        temperatures = list(range(1000, 100, -100))
-        for mcstep, temperature in enumerate(temperatures):
-            self.data.append(mcstep, 'temperature', temperature)
+        row_data = OrderedDict()
+        row_data['temperature'] = 100.0
+        for mctrial in range(10, 100, 10):
+            self.data.append(mctrial, row_data)
         self.data.reset()
         self.assertEqual(len(self.data), 0)
 
@@ -184,11 +165,11 @@ class TestDataContainer(unittest.TestCase):
         """
         Test total number of rows is returned.
         """
-        temperatures = list(range(1000, 100, -100))
-        for mcstep, temperature in enumerate(temperatures):
-            self.data.append(mcstep, 'temperature', temperature)
-            self.data.update()
-        self.assertEqual(len(self.data), len(temperatures))
+        row_data = OrderedDict()
+        row_data['temperature'] = 100.0
+        for mctrial in range(10, 101, 10):
+            self.data.append(mctrial, row_data)
+        self.assertEqual(len(self.data), 10)
 
     def test_get_average(self):
         """
