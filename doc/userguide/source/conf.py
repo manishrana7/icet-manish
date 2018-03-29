8887914378229951d4ca493c731ad576c3c7c2c8
Paul Erhart <erhart@chalmers.se>
2018-03-29 22:56:41 +0200

    DOC: suppress source links

--- doc/userguide/source/conf.py
+++ doc/userguide/source/conf.py
@@ -40,6 +40,7 @@ project = re.search("__project__ = '(.*)'", lines).group(1)
 author = re.search("__maintainer__ = '(.*)'", lines).group(1)
 
 site_url = 'https://icet.materialsmodeling.org/'
+html_show_sourcelink = False
 html_logo = "_static/logo.png"
 html_favicon = "_static/logo.ico"
 html_theme = 'sphinx_rtd_theme'
