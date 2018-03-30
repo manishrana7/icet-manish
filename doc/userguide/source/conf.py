98baaa3772359c79bd2bc0c7c62c4669469aa724
Paul Erhart <erhart@chalmers.se>
2018-03-30 08:35:20 +0200

    DOC: suppress source views

--- doc/userguide/source/conf.py
+++ doc/userguide/source/conf.py
@@ -16,12 +16,15 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.todo',
     'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
     'sphinx.ext.graphviz',
     'sphinx_sitemap',
     'breathe',
     'sphinx.ext.intersphinx',
 ]
+
+# To be put back upon public code release
+#     'sphinx.ext.viewcode',
+# html_show_sourcelink = False
 
 graphviz_output_format = 'svg'
 templates_path = ['_templates']
@@ -40,7 +43,6 @@ project = re.search("__project__ = '(.*)'", lines).group(1)
 author = re.search("__maintainer__ = '(.*)'", lines).group(1)
 
 site_url = 'https://icet.materialsmodeling.org/'
-html_show_sourcelink = False
 html_logo = "_static/logo.png"
 html_favicon = "_static/logo.ico"
 html_theme = 'sphinx_rtd_theme'
