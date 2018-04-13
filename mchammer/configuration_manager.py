c4ceb58d8ba65fb22fa0c4591ab29d27ddfac5ae
angqvist <angqvist@chalmers.se>
2018-04-13 16:50:59 +0200

    #181 adding standard boilerplate stuff

--- mchammer/configuration_manager.py
+++ mchammer/configuration_manager.py
@@ -8,18 +8,24 @@ class ConfigurationManager:
 
     Parameters
     ----------
-
+    occupations : list of int
+        The occupation of the configurations. The integers
+        refer to elements via their number in the periodic table
     strict_occupations : list of list of int
         the strictest form of the allowed occupations.
     occupation_constraints : list of list of int
         optional occupation constraint to enfore a more stricter species
         occupation than what is allowed from the Calculator.
+    sublattices : list of list of int
+        the integers refer to lattice site indices in the configuration.
     """
 
-        def __init__(self, strict_occupations, occupation_constraints=None):
+        def __init__(self, occupations, strict_occupations, sublattices,  occupation_constraints=None):
+            
+            self._occupations = occupations            
+            self._sublattices = sublattices
 
-            self._configuration = calc.configuration
-
+            self.
             # internal representation
             # (strings are probably not preferable in actual implementation;
             # they are used here for clarity)
@@ -29,6 +35,18 @@ class ConfigurationManager:
             #                                'Au': [4, 5, 6, 7],
             #                                'H':  [8, 9, 10],
             #                                'X':  [11, 12, 13, 14]}
+
+
+        @property
+        def occupations(self):
+            """ The occupations of the configuration."""
+            return self._occupations.copy()
+
+        @property
+        def sublattices(self):
+            """ The sublattices of the configuration. """
+            return self._sublattices.copy()
+
         def get_swap_indices(self, sublattice: int):
             """
             Returns a tuple of two random indices in a specific sublattice.
