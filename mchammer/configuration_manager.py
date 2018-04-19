cf265adca5ad1f2791e4f3829b0d9eee4f6d4964
angqvist <angqvist@chalmers.se>
2018-04-19 10:57:03 +0200

    #181 wrote more tests and flake8 fixed

--- mchammer/configuration_manager.py
+++ mchammer/configuration_manager.py
@@ -14,7 +14,7 @@ class ConfigurationManager(object):
     occupations : list of int
         The occupation of the configurations. The integers
         refer to elements via their number in the periodic table
-    strict_occupations : list of list of int
+    strict_constraints : list of list of int
         the strictest form of the allowed occupations.
     occupation_constraints : list of list of int
         optional occupation constraint to enfore a more stricter species
@@ -29,15 +29,15 @@ class ConfigurationManager(object):
       occupation constraint.
     """
 
-        
-    def __init__(self, occupations, strict_occupations, sublattices,  occupation_constraints=None):
+    def __init__(self, occupations, strict_constraints, sublattices,
+                 occupation_constraints=None):
 
         self._occupations = occupations
         self._sublattices = sublattices
 
         if occupation_constraints is not None:
             self._check_occupation_constraint(
-                strict_occupations, occupation_constraints)
+                strict_constraints, occupation_constraints)
         self._occupation_constraints = occupation_constraints
         self._possible_elements = self._get_possible_elements()
         self._element_occupation = self._get_element_occupation()
@@ -79,23 +79,26 @@ class ConfigurationManager(object):
             element_occupations.append(element_dict)
         return element_occupations
 
-    def _check_occupation_constraint(self, strict_occupations, occupation_constraints):
+    def _check_occupation_constraint(self, strict_constraints,
+                                     occupation_constraints):
         """
         Checks that the user defined occupations constrains are stricter or
         as strict as the strict occupations.
 
         Parameters
         ----------
-        strict_occupations : list of list of int
+        strict_constraints : list of list of int
         occupation_constraints : list of list of int
         """
-        error_message = "strict occupations and occupation constraints must be equal length"
-        assert len(strict_occupations) == len(
-            occupation_constraints), error_message
 
-        for strict_occ, occ in zip(strict_occupations, occupation_constraints):
+        if not len(strict_constraints) == len(occupation_constraints):
+            raise ValueError(
+                "strict occupations and occupation"
+                " constraints must be equal length")
+
+        for strict_occ, occ in zip(strict_constraints, occupation_constraints):
             if not set(occ).issubset(strict_occ):
-                raise RuntTimeEerror(
+                raise ValueError(
                     "User defined occupation constraints must be "
                     "stricter or as strict as strict occupations constraints.")
 
@@ -124,26 +127,26 @@ class ConfigurationManager(object):
             The sublattice to pick indices from.
         """
 
-        index_1 = np.random.choice(self.sublattices[sublattice])
-        element_1 = self.occupations[index_1]
+        index_1 = random.choice(self.sublattices[sublattice])
         # assuming all sites in this sublattice have same allowed occupations
 
-        possible_swap_elements = set(self.occupation_constraints[index_1] ) - set(self.occupations[index_1])
-        total_number_of_indices=0
+        possible_swap_elements = set(
+            self.occupation_constraints[index_1]) - set(
+                self.occupations[index_1])
+        total_number_of_indices = 0
         for element in possible_swap_elements:
             total_number_of_indices += len(
                 self._element_occupation[sublattice][element])
-        index=random.randint(0, total_number_of_indices-1)
+        index = random.randint(0, total_number_of_indices-1)
         for element in possible_swap_elements:
             if index < len(self._element_occupation[sublattice][element]):
-                index_2=self._element_occupation[sublattice][element][index]
+                index_2 = self._element_occupation[sublattice][element][index]
                 break
             else:
                 index -= len(self._element_occupation[sublattice][element])
         else:
             raise SwapNotPossibleError
         return index_1, index_2
-
 
     def get_flip_index(self, sublattice: int):
         """
@@ -159,7 +162,8 @@ class ConfigurationManager(object):
 
         """
 
-        index=np.random.choice(self.sublattices[sublattice])
-        element=np.random.choice(
-            set(self.occupation_constraints[index]) - set(self.occupations[index]))
+        index = random.choice(self.sublattices[sublattice])
+        element = random.choice(
+            set(self.occupation_constraints[index]) - set(
+                self.occupations[index]))
         return index, element
--- tests/unittest/mc/test_configuration_manager.py
+++ tests/unittest/mc/test_configuration_manager.py
@@ -1,7 +1,6 @@
 import unittest
 from mchammer.configuration_manager import ConfigurationManager
 from ase.build import bulk
-from ase.atoms import Atoms
 
 
 class TestConfigurationManager(unittest.TestCase):
@@ -10,16 +9,66 @@ class TestConfigurationManager(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestConfigurationManager, self).__init__(*args, **kwargs)
         self.atoms = bulk("Al").repeat(3)
-        self.constraints = [[13,47 ]*len(self.atoms)]
+        self.constraints = [[13, 47]*len(self.atoms)]
         self.strict_constraints = self.constraints
         self.sublattices = [[i for i in range(len(self.atoms))]]
 
     def setUp(self):
-        self.cm = ConfigurationManager(self.atoms.numbers, self.strict_constraints, self.sublattices, self.constraints)
+        self.cm = ConfigurationManager(
+            self.atoms.numbers, self.strict_constraints, self.sublattices,
+            self.constraints)
 
     def test_type(self):
         """ Test cm type """
         self.assertIsInstance(self.cm, ConfigurationManager)
+
+    def test_check_occupation_constraint(self):
+        """ Test the check occupation constraint method. """
+
+        # Check that equal constraint is allowed
+        constraint = [[1, 2], [1, 2], [1], [2]]
+        strict_constraint = [[1, 2], [1, 2], [1], [2]]
+        self.cm._check_occupation_constraint(strict_constraint, constraint)
+
+        # Check that a more accepting constraint throws ValueError
+        constraint = [[1, 2], [1, 2], [1], [2, 3]]
+        strict_constraint = [[1, 2], [1, 2], [1], [2]]
+        with self.assertRaises(Exception) as context:
+            self.cm._check_occupation_constraint(strict_constraint, constraint)
+
+        self.assertTrue("User defined occupation constraints must be "
+                        "stricter or as strict as strict"
+                        " occupations constraints." in str(context.exception))
+
+        # Check that the length of the constraints throw a value error
+        constraint = [[1, 2], [1, 2], [1]]
+        strict_constraint = [[1, 2], [1, 2], [1], [2]]
+        with self.assertRaises(Exception) as context:
+            self.cm._check_occupation_constraint(strict_constraint, constraint)
+
+        self.assertTrue("strict occupations and occupation "
+                        "constraints must be equal length"
+                        in str(context.exception))
+
+    def test_property_occupations(self):
+        """
+        Test that the occupation property returns expected result
+        and that it cannot be modified.
+        """
+
+        self.assertListEqual(list(self.cm.occupations),
+                             list(self.atoms.numbers))
+
+        # Test that the property can not be set.
+        with self.assertRaises(AttributeError) as context:
+            self.cm.occupations = []
+        self.assertTrue("can't set attribute" in str(context.exception))
+
+        # Test that the property can't be set by modifying the
+        # returned occupations
+        occupations = self.cm.occupations
+        occupations[0] = occupations[0] + 1
+        self.assertNotEqual(list(occupations), list(self.cm.occupations))
 
 
 if __name__ == '__main__':
