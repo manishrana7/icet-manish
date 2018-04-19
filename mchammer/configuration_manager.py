dbeb4090644a464c8e714b3a1bbbedb16dd8498c
angqvist <angqvist@chalmers.se>
2018-04-19 10:03:42 +0200

    #181 ran unit test and fixed bugs in CM

--- mchammer/configuration_manager.py
+++ mchammer/configuration_manager.py
@@ -29,138 +29,137 @@ class ConfigurationManager(object):
       occupation constraint.
     """
 
-        def __init__(self, occupations, strict_occupations, sublattices,  occupation_constraints=None):
+        
+    def __init__(self, occupations, strict_occupations, sublattices,  occupation_constraints=None):
 
-            self._occupations = occupations
-            self._sublattices = sublattices
+        self._occupations = occupations
+        self._sublattices = sublattices
 
-            if occupation_constraints is not None:
-                self._check_occupation_constraint(
-                    strict_occupations, occupation_constraints)
-            self._occupation_constraints = occupation_constraints
-            self._possible_elements = self._get_possible_elements()
-            self._element_occupation = self._get_element_occupation()
+        if occupation_constraints is not None:
+            self._check_occupation_constraint(
+                strict_occupations, occupation_constraints)
+        self._occupation_constraints = occupation_constraints
+        self._possible_elements = self._get_possible_elements()
+        self._element_occupation = self._get_element_occupation()
 
-            # internal representation
-            # (strings are probably not preferable in actual implementation;
-            # they are used here for clarity)
-            # ConfigurationManager._sublattices = [['Pd', 'Au'],
-            #                                      ['H',  'X']]
-            # ConfigurationManager._sites = {'Pd': [0, 1, 2, 3],
-            #                                'Au': [4, 5, 6, 7],
-            #                                'H':  [8, 9, 10],
-            #                                'X':  [11, 12, 13, 14]}
+        # internal representation
+        # (strings are probably not preferable in actual implementation;
+        # they are used here for clarity)
+        # ConfigurationManager._sublattices = [['Pd', 'Au'],
+        #                                      ['H',  'X']]
+        # ConfigurationManager._sites = {'Pd': [0, 1, 2, 3],
+        #                                'Au': [4, 5, 6, 7],
+        #                                'H':  [8, 9, 10],
+        #                                'X':  [11, 12, 13, 14]}
 
-        def _get_possible_elements(self):
-            """
-            Return a list of the possible elements in
-            the entire configuration.
-            """
-            possible_element = set()
-            for occ in self.occupation_constraints:
-                for element in occ:
-                    possible_element.insert(element)
+    def _get_possible_elements(self):
+        """
+        Return a list of the possible elements in
+        the entire configuration.
+        """
+        possible_element = set()
+        for occ in self.occupation_constraints:
+            for element in occ:
+                possible_element.add(element)
 
-            return list(possible_element)
+        return list(possible_element)
 
-        def _get_element_occupation(self):
-            """
-            Return the element occupation, i.e. the element -> lattice sites dict.
-            """
-            element_occupations = []
-            for i, sublattice in enumerate(self.sublattices)
-                element_dict = {}
-                for element in self._possible_elements:
-                    element_dict[element] = []
-                for lattice_site in sublattice:
-                    element = self.occupations[lattice_site]
-                    element_dict[element].push_back(lattice_site)
-                element_occupations.push_back(element_dict)
-            return element_occupations
-        def _check_occupation_constraint(self, strict_occupations, occupation_constraints):
-            """
-            Checks that the user defined occupations constrains are stricter or
-            as strict as the strict occupations.
+    def _get_element_occupation(self):
+        """
+        Return the element occupation, i.e. the element -> lattice sites dict.
+        """
+        element_occupations = []
+        for i, sublattice in enumerate(self.sublattices):
+            element_dict = {}
+            for element in self._possible_elements:
+                element_dict[element] = []
+            for lattice_site in sublattice:
+                element = self.occupations[lattice_site]
+                element_dict[element].append(lattice_site)
+            element_occupations.append(element_dict)
+        return element_occupations
 
-            Parameters
-            ----------
-            strict_occupations : list of list of int
-            occupation_constraints : list of list of int
-            """
-            error_message = "strict occupations and occupation constraints must be equal length"
-            assert len(strict_occupations) == len(
-                occupation_constraints), error_message
+    def _check_occupation_constraint(self, strict_occupations, occupation_constraints):
+        """
+        Checks that the user defined occupations constrains are stricter or
+        as strict as the strict occupations.
 
-            for strict_occ, occ in zip(strict_occupations, occupation_constraints):
-                if not set(occ).issubset(strict_occ):
-                    raise RuntTimeEerror(
-                        "User defined occupation constraints must be "
-                        "stricter or as strict as strict occupations constraints.")
+        Parameters
+        ----------
+        strict_occupations : list of list of int
+        occupation_constraints : list of list of int
+        """
+        error_message = "strict occupations and occupation constraints must be equal length"
+        assert len(strict_occupations) == len(
+            occupation_constraints), error_message
 
-        @property
-        def occupations(self):
-            """ The occupations of the configuration."""
-            return self._occupations.copy()
+        for strict_occ, occ in zip(strict_occupations, occupation_constraints):
+            if not set(occ).issubset(strict_occ):
+                raise RuntTimeEerror(
+                    "User defined occupation constraints must be "
+                    "stricter or as strict as strict occupations constraints.")
 
-        @property
-        def occupation_constraints(self):
-            """ The occupation constraints for this configuration manager."""
-            return self._occupation_constraints.copy()
+    @property
+    def occupations(self):
+        """ The occupations of the configuration."""
+        return self._occupations.copy()
 
-        @property
-        def sublattices(self):
-            """ The sublattices of the configuration. """
-            return self._sublattices.copy()
+    @property
+    def occupation_constraints(self):
+        """ The occupation constraints for this configuration manager."""
+        return self._occupation_constraints.copy()
 
-        def get_swap_indices(self, sublattice: int):
-            """
-            Returns a tuple of two random indices in a specific sublattice.
+    @property
+    def sublattices(self):
+        """ The sublattices of the configuration. """
+        return self._sublattices.copy()
 
-            Parameters
-            ----------
-            sublattice : int
-                The sublattice to pick indices from.
-            """
+    def get_swap_indices(self, sublattice: int):
+        """
+        Returns a tuple of two random indices in a specific sublattice.
 
-            index_1 = np.random.choice(self.sublattices[sublattice])
-            element_1 = self.occupations[index_1]
-            # assuming all sites in this sublattice have same allowed occupations
+        Parameters
+        ----------
+        sublattice : int
+            The sublattice to pick indices from.
+        """
 
-            possible_swap_elements = set(self.occupation_constraints[index_1]) - set(self.occupations[index_1]))
-            total_number_of_indices = 0
-            for element in possible_swap_elements:
-                total_number_of_indices += len(self._element_occupation[sublattice][element])
-            index = random.randint(0, total_number_of_indices-1)
-            for element in possible_swap_elements:
-                if index < len(self._element_occupation[sublattice][element]):
-                    index_2 = self._element_occupation[sublattice][element][index]
-                    break
-                else:
-                    index -= len(self._element_occupation[sublattice][element])
+        index_1 = np.random.choice(self.sublattices[sublattice])
+        element_1 = self.occupations[index_1]
+        # assuming all sites in this sublattice have same allowed occupations
+
+        possible_swap_elements = set(self.occupation_constraints[index_1] ) - set(self.occupations[index_1])
+        total_number_of_indices=0
+        for element in possible_swap_elements:
+            total_number_of_indices += len(
+                self._element_occupation[sublattice][element])
+        index=random.randint(0, total_number_of_indices-1)
+        for element in possible_swap_elements:
+            if index < len(self._element_occupation[sublattice][element]):
+                index_2=self._element_occupation[sublattice][element][index]
+                break
             else:
-                raise SwapNotPossibleError
-            return index_1, index_2
-            
-                
-            self._element_occupation[sublattice][]
+                index -= len(self._element_occupation[sublattice][element])
+        else:
+            raise SwapNotPossibleError
+        return index_1, index_2
 
-            raise NotImplementedError
 
-        def get_flip_index(self, sublattice: int):
-            """
-            Returns a tuple of a random index in a specific sublattice
-            and the element to flip it to.
+    def get_flip_index(self, sublattice: int):
+        """
+        Returns a tuple of a random index in a specific sublattice
+        and the element to flip it to.
 
-            Parameters
-            ----------
-            sublattice : int
-                The sublattice to pick indices from.
+        Parameters
+        ----------
+        sublattice : int
+            The sublattice to pick indices from.
 
-            return : (int, int) (index, element)
+        return : (int, int) (index, element)
 
-            """
+        """
 
-            index=np.random.choice(self.sublattices[sublattice])
-            element=np.random.choice(
-                set(self.occupation_constraints[index]) - set(self.occupations[index]))
-            return index, element
+        index=np.random.choice(self.sublattices[sublattice])
+        element=np.random.choice(
+            set(self.occupation_constraints[index]) - set(self.occupations[index]))
+        return index, element
--- tests/unittest/mc/test_configuration_manager.py
+++ tests/unittest/mc/test_configuration_manager.py
@@ -10,9 +10,17 @@ class TestConfigurationManager(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestConfigurationManager, self).__init__(*args, **kwargs)
         self.atoms = bulk("Al").repeat(3)
-        self.constraints
+        self.constraints = [[13,47 ]*len(self.atoms)]
+        self.strict_constraints = self.constraints
+        self.sublattices = [[i for i in range(len(self.atoms))]]
 
     def setUp(self):
-        
+        self.cm = ConfigurationManager(self.atoms.numbers, self.strict_constraints, self.sublattices, self.constraints)
+
+    def test_type(self):
+        """ Test cm type """
+        self.assertIsInstance(self.cm, ConfigurationManager)
 
 
+if __name__ == '__main__':
+    unittest.main()
