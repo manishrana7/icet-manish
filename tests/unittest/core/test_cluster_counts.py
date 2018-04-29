0190564562ba07cb7b3e932a30d8a609861cfc51
Paul Erhart <erhart@chalmers.se>
2018-04-29 15:51:14 +0200

    MAINT: properly encapsulated print_overview in cluster count test

--- tests/unittest/core/test_cluster_counts.py
+++ tests/unittest/core/test_cluster_counts.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
+import sys
 import unittest
 
 from ase.build import bulk
-from icet.core.cluster_counts import ClusterCounts
+from icet import Structure
 from icet.core.cluster import Cluster
+from icet.core.cluster_counts import ClusterCounts
 from icet.core.lattice_site import LatticeSite
+from icet.core.many_body_neighbor_list import ManyBodyNeighborList
 from icet.core.neighbor_list import get_neighbor_lists
 from icet.core.orbit_list import OrbitList
-from icet.core.many_body_neighbor_list import ManyBodyNeighborList
-from icet import Structure
+from io import StringIO
 
 
 def strip_surrounding_spaces(input_string):
@@ -22,7 +24,6 @@ def strip_surrounding_spaces(input_string):
     str
         original string minus surrounding spaces and empty lines
     '''
-    from io import StringIO
     s = []
     for line in StringIO(input_string):
         if len(line.strip()) == 0:
@@ -223,7 +224,12 @@ Ni   Ni    13
         """
         self.cluster_counts.count_clusters(self.structure,
                                            self.orbit_list, False)
+        # create StringIO object and redirect stdout.
+        capturedOutput = StringIO()
+        sys.stdout = capturedOutput
         self.cluster_counts.print_overview()  # call method
+        sys.stdout = sys.__stdout__  # reset redirect
+        self.assertTrue('Cluster Counts' in capturedOutput.getvalue())
 
 
 if __name__ == '__main__':
