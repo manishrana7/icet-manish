9361ced94a95518df113e59ddcbe4dab09126533
angqvist <angqvist@chalmers.se>
2018-05-07 13:17:08 +0200

    #163 flake8 fix

--- mchammer/ensembles/canonical_ensemble.py
+++ mchammer/ensembles/canonical_ensemble.py
@@ -13,7 +13,7 @@ class CanonicalEnsemble(BaseEnsemble):
     -----------
     temperature : float
         temperature in Kelvin.
-    boltzmann_constant : float (default is ev/K)    
+    boltzmann_constant : float (default is ev/K)
     """
 
     def __init__(self, atoms=None, calculator=None, name='Canonical Ensemble',
@@ -36,7 +36,8 @@ class CanonicalEnsemble(BaseEnsemble):
         self.total_trials += 1
 
         sublattice_index = self.get_random_sublattice_index()
-        indices, elements = self.configuration.get_swapped_state(sublattice_index)
+        indices, elements = \
+            self.configuration.get_swapped_state(sublattice_index)
 
         energy_diff = self.get_property_change(indices, elements)
 
@@ -57,5 +58,6 @@ class CanonicalEnsemble(BaseEnsemble):
         if energy_diff < 0:
             return True
         else:
-            return np.exp(-energy_diff/(self._boltzmann_constant * self.temperature)) > \
+            return np.exp(-energy_diff/(
+                self._boltzmann_constant * self.temperature)) > \
                 self.next_random_number()
