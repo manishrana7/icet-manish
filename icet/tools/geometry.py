96e041c9012093f282a4884b5919deb9c91d087d
Mattias Ångqvist <angqvist@chalmers.se>
2018-05-08 12:55:27 +0000

    Update geometry.py

--- icet/tools/geometry.py
+++ icet/tools/geometry.py
@@ -6,53 +6,6 @@ from icet.core.lattice_site import LatticeSite
 from icet.core_py.lattice_site import LatticeSite as LatticeSite_py
 
 
-def get_scaled_positions(positions, cell, wrap=True, pbc=[True, True, True]):
-    '''Get positions in reduced (scaled) coordinates.
-
-    If wrap is True, positions outside the unit cell will be wrapped into
-    the cell in the directions with periodic boundary conditions
-    such that the scaled coordinates are between zero and one.
-    '''
-
-    fractional = np.linalg.solve(cell.T, positions.T).T
-
-    if wrap:
-        for i, periodic in enumerate(pbc):
-            if periodic:
-                # Yes, we need to do it twice.
-                # See the scaled_positions.py test.
-                fractional[:, i] %= 1.0
-                fractional[:, i] %= 1.0
-
-    return fractional
-
-
-def find_lattice_site_from_position_python(structure, position):
-    '''
-    Get lattice neighbor from position.
-
-    This is the Python version of
-    `structure.findLatticeSiteFromPosition(position)`
-
-    It is slower but kept for debugging and if further development is needed.
-    '''
-
-    fractional = np.linalg.solve(structure.cell.T, np.array(position).T).T
-    unit_cell_offset = [int(round(x)) for x in fractional]
-
-    residual = np.dot(fractional - unit_cell_offset, structure.cell)
-    try:
-        index = structure.find_index_of_position(residual)
-    except Exception:
-        msg = ['error did not find index with pos: {}'.format(residual)]
-        msg += ['position in structure are:']
-        msg += ['\n' + str(structure.positions)]
-        raise Exception(' '.join(msg))
-
-    latNbr = LatticeSite(index, unit_cell_offset)
-    return latNbr
-
-
 def add_vacuum_in_non_pbc(atoms):
     '''
     Add vacuum in non-periodic directions.
@@ -129,42 +82,6 @@ def get_fractional_positions_from_neighbor_list(structure, neighbor_list):
             structure.cell, wrap=False,
             pbc=structure.pbc)
     return fractional_positions
-
-
-def get_permutation_matrix(input_configuration,
-                           reference_structure,
-                           tolerance_cell=0.05,
-                           ):
-    '''
-    Computes and returns the permutation
-    matrix that takes the reference cell to the input cell,
-    i.e. permutation_matrix * reference_cell = input_cell
-    '''
-
-    input_cell = input_configuration.cell
-
-    # obtain the (in general non-integer) transformation matrix
-    # connecting the input configuration to the reference structure
-    # L = L_p.P --> P = L_p^-1.L
-    P = np.dot(input_cell, np.linalg.inv(reference_structure.cell))
-
-    # assert that the transformation matrix does not deviate too
-    # strongly from the nearest integer matrix
-    if np.linalg.norm(P - np.around(P)) / 9 > tolerance_cell:
-        s = 'Failed to map configuration to reference'
-        s += 'structure (tolerance_cell exceeded).\n'
-        s += 'reference:\n {}\n'.format(reference_structure.cell)
-        s += 'input:\n {}\n'.format(input_configuration.cell)
-        s += 'input_cell:\n {}\n'.format(input_cell)
-        s += 'P:\n {}\n'.format(P)
-        s += 'P_round:\n {}\n'.format(np.around(P))
-        s += 'Deviation: {}\n'.format(np.linalg.norm(P - np.around(P)) / 9)
-        s += 'You can try raising `tolerance_cell`.'
-        raise Exception(s)
-
-    # reduce the (real) transformation matrix to the nearest integer one
-    P = np.around(P)
-    return P
 
 
 def get_fractional_positions_from_ase_neighbor_list(atoms, neighbor_list):
