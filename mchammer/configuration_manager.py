25517ab1b94e712eba001879cadcc13a46822038
angqvist <angqvist@chalmers.se>
2018-05-15 16:13:10 +0200

    #163 setup -> set up

--- mchammer/configuration_manager.py
+++ mchammer/configuration_manager.py
@@ -47,10 +47,10 @@ class ConfigurationManager(object):
         else:
             occupation_constraints = strict_constraints
         self._occupation_constraints = occupation_constraints
-        self._possible_elements = self._setup_possible_elements()
+        self._possible_elements = self._set_up_possible_elements()
         self._element_occupation = self._get_element_occupation()
 
-    def _setup_possible_elements(self)->List[int]:
+    def _set_up_possible_elements(self)->List[int]:
         """
         Return a list of the possible elements in
         the entire configuration.
