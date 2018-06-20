65bc158467487fc550ab3e957c95697fb057c16a
Magnus Rahm <rmagnus@chalmers.se>
2018-06-20 10:07:16 +0200

    MAINT: Import VCSGC ensemble

--- mchammer/ensembles/__init__.py
+++ mchammer/ensembles/__init__.py
@@ -2,5 +2,7 @@
 
 from .canonical_ensemble import CanonicalEnsemble
 from .semi_grand_canonical_ensemble import SemiGrandCanonicalEnsemble
+from .vcsgc_ensemble import VCSGCEnsemble
 
-__all__ = ['CanonicalEnsemble', 'SemiGrandCanonicalEnsemble']
+__all__ = ['CanonicalEnsemble', 'SemiGrandCanonicalEnsemble',
+           'VCSGCEnsemble']
