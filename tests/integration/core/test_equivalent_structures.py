0cd80bdf5fb8b354b5bf5b4428f8baaa878af6ee
Magnus Rahm <rmagnus@chalmers.se>
2018-06-25 15:24:08 +0200

    TST: Replaced equivalent structure test with a sweeter one

---
+++ tests/integration/core/test_equivalent_structures.py
@@ -0,0 +1,24 @@
+from icet import ClusterSpace
+from ase.build import bulk
+from ase.db import connect
+import numpy as np
+
+prim = bulk('Au', a=4.0, crystalstructure='hcp')
+cutoffs = [7.0, 7.0, 7.0]
+subelements = ['Au', 'Pd']
+cs = ClusterSpace(prim, cutoffs, subelements)
+
+db = connect('../../equivalent_structure_pairs.db')
+
+# Loop over all pairs
+for structure in db.select():
+
+    # Do not check the pair that was just checked
+    if structure.equivalent_structure < structure.id:
+        continue
+
+    cv_1 = cs.get_cluster_vector(structure.toatoms())
+    cv_2 = cs.get_cluster_vector(
+        db.get(structure.equivalent_structure).toatoms())
+
+    assert np.all(np.abs(cv_2 - cv_1) < 1e-6)
