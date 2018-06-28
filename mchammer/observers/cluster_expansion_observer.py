42f8cc3aef8b8abdbb8b8e90650b0803fe84395a
williamz <william.munoz@chalmers.se>
2018-06-28 14:49:56 +0200

    TEST: Unittest added for ClusterExpansionObserver

--- mchammer/observers/cluster_expansion_observer.py
+++ mchammer/observers/cluster_expansion_observer.py
@@ -13,9 +13,12 @@ class ClusterExpansionObserver(BaseObserver):
     atoms : :class:`ase:Atoms`
         atomic structure to be used as a reference by the observer.
     cluster_expansion :class:`icet:ClusterExpansion` 
-        
+        cluster expansion model to be used for observation
     tag : str (default: `ClusterExpansionObserver`)
         human readable observer name
+    interval : int
+        interval at which observation is done during the Monte Carlo
+        simulation
 
     Attributes
     ----------
@@ -25,16 +28,29 @@ class ClusterExpansionObserver(BaseObserver):
         observation interval
     """
 
-    def __init__(self, atoms, cluster_expansion,
-                 tag='ClusterExpansionObserver', interva),
-                 atoms, cluster_expansion):
-        super().__init__(interval=interval, return_type=return_type, tag=tag)
+    def __init__(self, atoms, cluster_expansion, scaling=None,
+                 interval=None, tag='ClusterExpansionObserver'):
+        super().__init__(interval=interval, return_type=float, tag=tag)
         self._cluster_expansion = cluster_expansion
-        self.atom = atom
+        self.atoms = atoms
+
+        if interval is None:
+            raise ValueError("The value of interval must be specified")
+
+        if scaling is None:
+            self._property_scaling = len(atoms)
+        else:
+            self._property_scaling = scaling
 
     def get_observable(self, occupations: List[int]) -> float:
         """
-        Returns an observation.
+        Returns the value of a property from a cluster expansion model.
+
+        Parameters
+        ----------
+        occupations
+            list of atomic species or vector occupation of the atomic
+            configuration.
         """
         self.atoms.set_atomic_numbers(occupations)
         return self.cluster_expansion.predict(self.atoms) * \
