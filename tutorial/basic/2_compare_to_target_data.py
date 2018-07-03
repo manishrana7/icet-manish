7d290714a0ea7a76cc45036e7b71c3ceefb4a6bf
Paul Erhart <erhart@chalmers.se>
2018-07-03 13:27:30 +0200

    MAINT: removed pandas dependency from 2_compare_to_target_data.py

--- tutorial/basic/2_compare_to_target_data.py
+++ tutorial/basic/2_compare_to_target_data.py
@@ -3,7 +3,6 @@
 import matplotlib.pyplot as plt
 from ase.db import connect
 from icet import ClusterExpansion
-from pandas import DataFrame
 
 # step 1: Compile predicted and reference data for plotting
 ce = ClusterExpansion.read('mixing_energy.ce')
@@ -11,9 +10,9 @@ data = {'concentration': [], 'reference_energy': [], 'predicted_energy': []}
 db = connect('reference_data.db')
 for row in db.select('natoms<=6'):
     data['concentration'].append(row.concentration)
-    data['reference_energy'].append(row.mixing_energy)
-    data['predicted_energy'].append(ce.predict(row.toatoms()))
-data = DataFrame.from_dict(data)
+    # the factor of 1e3 serves to convert from eV/atom to meV/atom
+    data['reference_energy'].append(1e3*row.mixing_energy)
+    data['predicted_energy'].append(1e3*ce.predict(row.toatoms()))
 
 # step 2: Plot results
 fig, ax = plt.subplots(figsize=(4, 3))
@@ -21,8 +20,8 @@ ax.set_xlabel(r'Concentration')
 ax.set_ylabel(r'Mixing energy (meV/atom)')
 ax.set_xlim([0, 1])
 ax.set_ylim([-69, 15])
-ax.scatter(data['concentration'], 1e3 * data['reference_energy'],
+ax.scatter(data['concentration'], data['reference_energy'],
            marker='o', label='reference')
-ax.scatter(data['concentration'], 1e3 * data['predicted_energy'],
+ax.scatter(data['concentration'], data['predicted_energy'],
            marker='x', label='CE prediction')
 plt.savefig('mixing_energy_comparison.png', bbox_inches='tight')
