dbde5e368dc9bfee6529094f73359c35528ad72d
williamz <william.munoz@chalmers.se>
2018-07-12 18:35:25 +0200

    TST: Refactor unittest for ClusterExpansion

--- tests/unittest/core/test_cluster_expansion.py
+++ tests/unittest/core/test_cluster_expansion.py
@@ -1,11 +1,5 @@
-"""
-This test checks that a cluster expansion model can be initialized
-with any structure in the test database and can predict a property.
-"""
 import unittest
 import tempfile
-
-from ase.db import connect
 from icet import ClusterSpace, ClusterExpansion
 from ase.build import bulk
 from io import StringIO
@@ -30,101 +24,75 @@ def strip_surrounding_spaces(input_string):
 
 
 class TestClusterExpansion(unittest.TestCase):
-    """
-    Container for tests of the class functionality
-    """
+    """Container for tests of the class functionality."""
 
     def __init__(self, *args, **kwargs):
         super(TestClusterExpansion, self).__init__(*args, **kwargs)
-        self.db = connect('structures_for_testing.db')
-        self.subelements = ['H', 'He', 'Pb']
-        self.cutoffs = [1.4] * 3
+        self.atoms = bulk('Au')
+        cutoffs = [3.0] * 3
+        chemical_symbols = ['Au', 'Pd']
+        self.cs = ClusterSpace(self.atoms, cutoffs, chemical_symbols)
+
+    def setUp(self):
+        """Set up before each test."""
+        params_len = self.cs.get_cluster_space_size()
+        self.parameters = list(range(params_len))
+        self.ce = ClusterExpansion(self.cs, self.parameters)
 
     def test_init(self):
         """Test that initialization works."""
-        atoms = bulk('Au')
-        cutoffs = [3.0]
-        cs = ClusterSpace(atoms, cutoffs, ['Au', 'Pd'])
+        self.assertIsInstance(self.ce, ClusterExpansion)
+
+        # test whether method raises Exception
         with self.assertRaises(ValueError) as context:
-            ClusterExpansion(cs, [0.0])
-        msg = 'cluster_space and parameters must have the same' + \
-            ' length (3 != 1)'
-        self.assertEqual(str(context.exception), msg)
+            ClusterExpansion(self.cs, [0.0])
+        self.assertTrue('cluster_space and parameters must have the same'
+                        ' length (5 != 1)' in str(context.exception))
 
-    def test_all_systems_in_database(self):
-        """
-        Test all structures in database
-        """
-        for row in self.db.select():
-            atoms_row = row.toatoms()
-            atoms_row.set_chemical_symbols(
-                len(atoms_row) * [self.subelements[0]])
-            self._test_clusterexpansion_model(
-                atoms_row, self.cutoffs, self.subelements)
+    def test_predict(self):
+        """ Test predict function"""
+        predicted_val = self.ce.predict(self.atoms)
+        self.assertEqual(predicted_val, 10.0)
 
-    def _test_clusterexpansion_model(self, atoms, cutoffs, subelements):
-        """
-        Test cluster expansion init and prediction
+    def test_property_clusterspace(self):
+        """Test cluster space property."""
+        self.assertEqual(self.ce.cluster_space, self.cs)
 
-        Parameters
-        ----------
-        atoms : ASE Atoms object
-            atomic configuration
-        subelements : list of strings (chemical symbols)
-            list of elements that are allowed
-        """
-        cs = ClusterSpace(atoms, cutoffs, subelements)
-        params_len = cs.get_cluster_space_size()
-        params = list(range(params_len))
+    def test_property_parameters(self):
+        """Test parameters properties."""
+        self.assertEqual(self.ce.parameters, self.parameters)
 
-        ce = ClusterExpansion(cs, params)
-        predicted_val = ce.predict(atoms)
-
-        self.assertIsInstance(predicted_val, float)
+    def test_len(self):
+        """Test len functionality."""
+        self.assertEqual(self.ce.__len__(), len(self.parameters))
 
     def test_read_write(self):
-        """
-        Test read write functionality.
-        """
-        atoms = bulk("Al")
-        cluster_space = ClusterSpace(atoms, self.cutoffs, self.subelements)
-        params = list(range(len(cluster_space)))
-        ce = ClusterExpansion(cluster_space, params)
+        """Test read and write functionalities."""
+        # save to file
+        temp_file = tempfile.NamedTemporaryFile()
+        self.ce.write(temp_file.name)
 
-        f = tempfile.NamedTemporaryFile()
-        ce.write(f.name)
-        f.seek(0)
-        ce_read = ClusterExpansion.read(f.name)
+        # read from file
+        temp_file.seek(0)
+        ce_read = ClusterExpansion.read(temp_file.name)
 
-        # Test clusterspace in cluster expansion
-        self.assertEqual(cluster_space._atoms,
-                         ce_read.cluster_space._atoms)
-        self.assertEqual(list(cluster_space._cutoffs),
-                         list(ce_read.cluster_space._cutoffs))
-        self.assertEqual(cluster_space._chemical_symbols,
+        # check cluster space
+        self.assertEqual(self.cs._atoms, ce_read.cluster_space._atoms)
+        self.assertEqual(self.cs._cutoffs, ce_read.cluster_space._cutoffs)
+        self.assertEqual(self.cs._chemical_symbols,
                          ce_read.cluster_space._chemical_symbols)
-        self.assertEqual(cluster_space._mi, ce_read.cluster_space._mi)
-        self.assertEqual(cluster_space._verbosity,
-                         ce_read.cluster_space._verbosity)
+        self.assertEqual(self.cs._mi, ce_read.cluster_space._mi)
 
-        # Test equal parameters
-        self.assertEqual(ce_read.parameters, params)
+        # check parameters
+        self.assertEqual(ce_read.parameters, self.parameters)
 
     def test_repr(self):
-        """
-        Testing repr functionality
-        """
-        atoms = bulk("Al")
-        cutoffs = [3.0] * 3
-        subelements = ['Al', 'Pd']
-        cluster_space = ClusterSpace(atoms, cutoffs, subelements)
-        params = list(range(len(cluster_space)))
-        ce = ClusterExpansion(cluster_space, params)
+        """Testing repr functionality."""
 
-        retval = ce.__repr__()
+        retval = self.ce.__repr__()
         target = """
 ================================= Cluster Expansion =================================
- chemical species: Al Pd
+ chemical species: Pd Au
  cutoffs: 3.0000 3.0000 3.0000
  total number of orbits: 5
  number of orbits by order: 0= 1  1= 1  2= 1  3= 1  4= 1
@@ -133,30 +101,22 @@ index | order |  radius  | multiplicity | orbit_index | multi_component_vector |
 -------------------------------------------------------------------------------------
    0  |   0   |   0.0000 |        1     |      -1     |           .            |  0  
    1  |   1   |   0.0000 |        1     |       0     |          [0]           |  1  
-   2  |   2   |   1.4319 |        6     |       1     |         [0, 0]         |  2  
-   3  |   3   |   1.6534 |        8     |       2     |       [0, 0, 0]        |  3  
-   4  |   4   |   1.7537 |        2     |       3     |      [0, 0, 0, 0]      |  4  
+   2  |   2   |   1.4425 |        6     |       1     |         [0, 0]         |  2  
+   3  |   3   |   1.6657 |        8     |       2     |       [0, 0, 0]        |  3  
+   4  |   4   |   1.7667 |        2     |       3     |      [0, 0, 0, 0]      |  4  
 =====================================================================================
 """ # noqa
         self.assertEqual(strip_surrounding_spaces(target),
                          strip_surrounding_spaces(retval))
 
     def test_get_string_representation(self):
-        """
-        Testing _get_string_representation functionality.
-        """
-        atoms = bulk("Al")
-        cutoffs = [3.0] * 3
-        subelements = ['Al', 'Pd']
-        cluster_space = ClusterSpace(atoms, cutoffs, subelements)
-        params = list(range(len(cluster_space)))
-        ce = ClusterExpansion(cluster_space, params)
+        """Testing _get_string_representation functionality."""
 
-        retval = ce._get_string_representation(print_threshold=2,
-                                               print_minimum=1)
+        retval = self.ce._get_string_representation(print_threshold=2,
+                                                    print_minimum=1)
         target = """
 ================================= Cluster Expansion =================================
- chemical species: Al Pd
+ chemical species: Pd Au
  cutoffs: 3.0000 3.0000 3.0000
  total number of orbits: 5
  number of orbits by order: 0= 1  1= 1  2= 1  3= 1  4= 1
@@ -165,7 +125,7 @@ index | order |  radius  | multiplicity | orbit_index | multi_component_vector |
 -------------------------------------------------------------------------------------
    0  |   0   |   0.0000 |        1     |      -1     |           .            |  0  
  ...
-   4  |   4   |   1.7537 |        2     |       3     |      [0, 0, 0, 0]      |  4  
+   4  |   4   |   1.7667 |        2     |       3     |      [0, 0, 0, 0]      |  4  
 =====================================================================================
 """ # noqa
         self.assertEqual(strip_surrounding_spaces(target),
