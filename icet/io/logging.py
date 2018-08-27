56ec3b91130ab4a43fd8821702b8be4220053ae2
williamz <william.munoz@chalmers.se>
2018-08-27 20:47:12 +0200

    MAINT: Added handler to main logger

--- icet/io/logging.py
+++ icet/io/logging.py
@@ -1,28 +1,24 @@
 import logging
 import sys
 
-
 # This is the root logger of icet
 logger = logging.getLogger('icet')
 
-# Set a format for the logger messages
+# Set the format which is simpler for the console
 FORMAT = '%(name)s: %(levelname)s  %(message)s'
-logging.basicConfig(level=logging.WARNING, format=FORMAT)
+formatter = logging.Formatter(FORMAT)
 
 # Will process all levels of WARNING or higher
 logger.setLevel(logging.WARNING)
-
-# Set the format which is simpler for the console
-formatter = logging.Formatter(FORMAT)
 
 # If you know what you are doing you may set this to True
 logger.propagate = False
 
 # The logger will collect events from children and the default
 # behaviour is to print it directly to stdout
-ch = logging.StreamHandler(sys.stdout)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def set_config(filename=None, level=None):
