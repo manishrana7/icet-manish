577ba99b267fba2165852630fb93c9201185d63a
Magnus Rahm <rmagnus@chalmers.se>
2018-09-13 09:36:56 +0200

    DOC: Updated tutorial to accomodate updated cluster counts module

--- examples/get_cluster_count.py
+++ examples/get_cluster_count.py
@@ -14,16 +14,14 @@ from icet.core.orbit_list import create_orbit_list
 prim_atoms = bulk('Ti', 'sc', a=3.0)
 atoms = prim_atoms.repeat([2, 1, 1])
 atoms.set_chemical_symbols(['Ti', 'W']*1)
+cutoffs = [5.0]
 # End setup
 
 # Determine the orbit list for the corresponding primitive structure for all
 # pair clusters within the cutoff distance
-cutoffs = [5.1]
 prim_orbitlist = create_orbit_list(prim_atoms, cutoffs)
-
 # Use the primitive orbit list to count the number of clusters.
 cluster_counts = ClusterCounts(prim_orbitlist, atoms)
-
 # Print all of the clusters that were found.
 print('Number of atoms: {0}'.format(len(atoms)))
 print('Found {} orbits'.format(len(cluster_counts)))
