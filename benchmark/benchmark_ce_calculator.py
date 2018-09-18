6d95ec80ae5a02725f26bb7aecf8d92dd7d36684
angqvist <angqvist@chalmers.se>
2018-09-18 16:35:17 +0200

    #174 updated benchmark ce calc.py

--- benchmark/benchmark_ce_calculator.py
+++ benchmark/benchmark_ce_calculator.py
@@ -14,7 +14,6 @@ def time_local_energy(calculator, iters):
         calculator.calculate_local_contribution(local_indices=indices,
                                                 occupations=occupations)
     t1 = time.time() - t0
-    t1 /= iters
     return t1
 
 
@@ -25,15 +24,37 @@ def time_total_energy(calculator, iters):
     for _ in range(iters):
         calculator.calculate_total(occupations=occupations)
     t1 = time.time() - t0
-    t1 /= iters
     return t1
 
 
+
+def print_timing_ratios(atoms, iters, sizes, cutoffs):
+    """ Get timing ratios between local and total energy calculations."""
+
+    print("# $1 size, $2 iters, $3 atom size, $4 ce calc init time (s), $5 t_local, $6 t_total, $7 t_total/t_local")
+    elements = ["Al", "Ga"]
+    cs = ClusterSpace(atoms, cutoffs, elements)
+    parameters = np.array([1.2 for _ in range(len(cs))])
+    ce = ClusterExpansion(cs, parameters)
+    for size in sizes:
+        atoms_cpy = atoms.repeat(size)
+        t0 = time.time()
+        calculator = ClusterExpansionCalculator(atoms_cpy, ce)
+        time_ce_init = time.time() - t0
+        
+        t_local = time_local_energy(calculator, iters)
+        t_total = time_total_energy(calculator, iters)
+        print(size, iters, len(atoms_cpy), time_ce_init, t_local, t_total, t_total/t_local)
+
+
 if __name__ == '__main__':
-    iters = 100
+    iters = 30
     atoms = bulk("Al")
     cutoffs = [10, 6, 5]
     elements = ["Al", "Ga"]
+    sizes = [2,4,5,6,7,8,9,10,16]
+    print_timing_ratios(atoms, iters,sizes, cutoffs)
+    # asd
     cs = ClusterSpace(atoms, cutoffs, elements)
     atoms = atoms.repeat(10)
     parameters = np.array([1.2 for _ in range(len(cs))])
@@ -51,5 +72,3 @@ if __name__ == '__main__':
 
     print("Time for calculating {} mc step ({}) local energies {}s".format(
         iters/len(atoms), len(atoms), t_local * len(calculator.atoms)))
-    print("Time for {} mc steps {} minutes".format(
-        iters/len(atoms),        t_local / 60 * iters/len(calculator.atoms)))
