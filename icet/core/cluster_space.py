971d9b82c18dad79da9e86a0278378ce33129f3f
angqvist <angqvist@chalmers.se>
2018-10-08 15:08:34 +0200

    #187 fixed python Ol issues in CS.py

--- icet/core/cluster_space.py
+++ icet/core/cluster_space.py
@@ -47,25 +47,25 @@ class ClusterSpace(_ClusterSpace):
         self._mi = Mi
 
         # set up orbit list
-        orbit_list = OrbitList(self._atoms, self._cutoffs)        
+        self._orbit_list = OrbitList(self._atoms, self._cutoffs)
 
         # handle occupations
         if Mi is None:
             Mi = len(chemical_symbols)
         if isinstance(Mi, dict):
             Mi = self._get_Mi_from_dict(Mi,
-                                        orbit_list.get_primitive_structure())
+                                        self._orbit_list.get_primitive_structure())
         if not isinstance(Mi, list):
             if isinstance(Mi, int):
-                Mi = [Mi] * len(orbit_list.get_primitive_structure())
+                Mi = [Mi] * len(self._orbit_list.get_primitive_structure())
             else:
                 raise Exception('Mi has wrong type (ClusterSpace)')
-        assert len(Mi) == len(orbit_list.get_primitive_structure()), \
+        assert len(Mi) == len(self._orbit_list.get_primitive_structure()), \
             'len(Mi) does not equal the number of sites' \
             ' in the primitive structure'
 
         # call (base) C++ constructor
-        _ClusterSpace.__init__(self, Mi, chemical_symbols, orbit_list)
+        _ClusterSpace.__init__(self, Mi, chemical_symbols, self._orbit_list)
 
     @staticmethod
     def _get_Mi_from_dict(Mi: dict, atoms: Union[Atoms, Structure]):
@@ -308,9 +308,7 @@ class ClusterSpace(_ClusterSpace):
     @property
     def orbit_list(self):
         """Orbit list that defines the cluster in the cluster space"""
-        ol_cpp = self._get_orbit_list()
-        
-
+        return self._get_orbit_list()
 
     def write(self, filename: str):
         """
@@ -436,9 +434,9 @@ def get_singlet_configuration(atoms: Atoms, to_primitive: bool=False) -> Atoms:
     else:
         singlet_configuration = atoms.copy()
         singlet_configuration = add_vacuum_in_non_pbc(singlet_configuration)
-        orbit_list = cluster_space.get_orbit_list()
+        # orbit_list = cluster_space.get_orbit_list()
         orbit_list_supercell \
-            = orbit_list.get_supercell_orbit_list(singlet_configuration)
+            = cluster_space._orbit_list.get_supercell_orbit_list(singlet_configuration)
         for singlet in cluster_data:
             for site in singlet['sites']:
                 element = chemical_symbols[singlet['orbit_index'] + 1]
