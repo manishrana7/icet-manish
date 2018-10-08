f9669f03b03efd1c7ed06096e85f0261f8760275
Paul Erhart <erhart@chalmers.se>
2018-10-08 17:49:18 +0200

    MAINT: fixed CamelCase to precomputeMultiComponentVectors

--- src/ClusterSpace.cpp
+++ src/ClusterSpace.cpp
@@ -38,7 +38,7 @@ ClusterSpace::ClusterSpace(std::vector<int> numberOfAllowedSpecies,
             _orbitList.removeOrbit(i);
         }
     }
-    precomputeMulticomponentVectors();
+    precomputeMultiComponentVectors();
 }
 
 /**
@@ -281,7 +281,7 @@ std::vector<int> ClusterSpace::getNumberOfAllowedSpeciesBySite(const Structure &
 }
 
 /// Precomputes permutations and multicomponent vectors of each orbit.
-void ClusterSpace::precomputeMulticomponentVectors()
+void ClusterSpace::precomputeMultiComponentVectors()
 {
     _clusterSpaceInfo.clear();
     std::vector<int> emptyVec = {0};
