20ad55e443eb98ade673f6e2b4e9a681726df53e
Magnus Rahm <rmagnus@chalmers.se>
2018-10-25 07:22:03 +0000

    Resolve "The documentation has yet to incorporate modifications of OrbitList and permuation maps"

--- src/PermutationMatrix.hpp
+++ src/PermutationMatrix.hpp
@@ -10,17 +10,13 @@
 #include <unordered_map>
 #include <algorithm>
 
-using namespace Eigen;
-
 namespace py = pybind11;
 
-///@TODO: Think about renaming this to permutation matrix since it doesnt really map anything
-
-class PermutationMap
+class PermutationMatrix
 {
   public:
-    PermutationMap(const std::vector<Vector3d> &translations,
-                   const std::vector<Matrix3d> &rotations)
+    PermutationMatrix(const std::vector<Eigen::Vector3d> &translations,
+                   const std::vector<Eigen::Matrix3d> &rotations)
     {
         _translations = translations;
         _rotations = rotations;
@@ -28,9 +24,9 @@ class PermutationMap
     }
 
     ///Build the permutationmap usng the input positions
-    void build(const Eigen::Matrix<double, Dynamic, 3, RowMajor> &positions);
+    void build(const Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> &positions);
 
-    std::vector<std::vector<Vector3d>> getPermutedPositions() const
+    std::vector<std::vector<Eigen::Vector3d>> getPermutedPositions() const
     {
         return _permutedPositions;
     }
@@ -39,16 +35,16 @@ class PermutationMap
      Returns indices for unique positions as well as the representative positions,
      same indices share the same position
     */
-    std::pair<std::vector<std::vector<int>>, std::vector<Vector3d>> getIndexedPermutedPositions()
+    std::pair<std::vector<std::vector<int>>, std::vector<Eigen::Vector3d>> getIndexedPermutedPositions()
     {
-        std::vector<Vector3d> uniquePositions;
+        std::vector<Eigen::Vector3d> uniquePositions;
         std::vector<std::vector<int>> indicePositions(_permutedPositions.size(), std::vector<int>(_permutedPositions[0].size()));
         for (size_t row = 0; row < _permutedPositions.size(); row++)
         {
             for (size_t col = 0; col < _permutedPositions[0].size(); col++)
             {
 
-                Vector3d pos = _permutedPositions[row][col];
+                Eigen::Vector3d pos = _permutedPositions[row][col];
                 const auto find = std::find(uniquePositions.begin(), uniquePositions.end(), pos);
                 if (find == uniquePositions.end())
                 {
@@ -66,16 +62,16 @@ class PermutationMap
     }
 
   private:
-    std::vector<Vector3d> _translations;
-    std::vector<Matrix3d> _rotations;
-    std::vector<std::vector<Vector3d>> _permutedPositions;
+    std::vector<Eigen::Vector3d> _translations;
+    std::vector<Eigen::Matrix3d> _rotations;
+    std::vector<std::vector<Eigen::Vector3d>> _permutedPositions;
 
     /** 
     Help function to round a Vector3d for easier comparing the transmutated positions
 
     @TODO: move this outside the class into some type of support/tools collection
     */
-    void roundVector3d(Vector3d &vec3d)
+    void roundVector3d(Eigen::Vector3d &vec3d)
     {
         vec3d[0] = round(vec3d[0] * 1.0 / symprec) / (1.0 / symprec);
         vec3d[1] = round(vec3d[1] * 1.0 / symprec) / (1.0 / symprec);
--- src/PyBinding.cpp
+++ src/PyBinding.cpp
@@ -2,7 +2,7 @@
 #include "NeighborList.hpp"
 #include "ManyBodyNeighborList.hpp"
 #include "Cluster.hpp"
-#include "PermutationMap.hpp"
+#include "PermutationMatrix.hpp"
 #include "LatticeSite.hpp"
 #include "ClusterCounts.hpp"
 #include "LocalOrbitListGenerator.hpp"
@@ -81,15 +81,9 @@ PYBIND11_MODULE(_icet, m)
            :members:
            :undoc-members:
 
-        OrbitList
-        ---------
-        .. autoclass:: OrbitList
-           :members:
-           :undoc-members:
-
         PermutationMatrix
-        --------------
-        .. autoclass:: PermutationMap
+        -----------------
+        .. autoclass:: PermutationMatrix
            :members:
            :undoc-members:
 
@@ -496,13 +490,13 @@ PYBIND11_MODULE(_icet, m)
         .def(py::self == py::self);
     ;
 
-    // @todo document PermutationMap in pybindings
-    py::class_<PermutationMap>(m, "PermutationMatrix")
+    // @todo document PermutationMatrix in pybindings
+    py::class_<::PermutationMatrix>(m, "PermutationMatrix")
         .def(py::init<const std::vector<Vector3d> &,
                       const std::vector<Matrix3d> &>())
-        .def("build", &PermutationMap::build)
-        .def("get_permuted_positions", &PermutationMap::getPermutedPositions)
-        .def("get_indexed_positions", &PermutationMap::getIndexedPermutedPositions)
+        .def("build", &::PermutationMatrix::build)
+        .def("get_permuted_positions", &::PermutationMatrix::getPermutedPositions)
+        .def("get_indexed_positions", &::PermutationMatrix::getIndexedPermutedPositions)
 
         ;
 
