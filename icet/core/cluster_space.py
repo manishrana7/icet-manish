afacf78dab986e40b07805234605779b1d150741
Paul Erhart <erhart@chalmers.se>
2018-11-13 20:15:29 +0000

    Update cluster_space.py

--- icet/core/cluster_space.py
+++ icet/core/cluster_space.py
@@ -39,7 +39,7 @@ class ClusterSpace(_ClusterSpace):
                  chemical_symbols: Union[List[str], List[List[str]]]) -> None:
 
         if not isinstance(atoms, Atoms):
-            raise TypeError('input configuration must be an ASE Atoms object')
+            raise TypeError('Input configuration must be an ASE Atoms object.')
 
         self._atoms = atoms.copy()
         self._cutoffs = cutoffs
