6374bf5aa744ca3b81f2974519478763b3321bf9
Thomas Holm Rod <thomas.rod@esss.se>
2018-11-30 00:07:20 +0100

    Fixing style issues

--- tests/unittest/test_icet/core_py/test_permutation_matrix.py
+++ tests/unittest/test_icet/core_py/test_permutation_matrix.py
@@ -13,7 +13,6 @@ from icet.core_py.lattice_site import LatticeSite
 #from icet.core.orbit_list import __get_lattice_site_permutation_matrix\
 #    as get_lattice_site_permutation_matrix
 
-
 lean_flag = False
 
 if lean_flag:
@@ -32,11 +31,9 @@ class TestPermutationMatrix(unittest.TestCase):
         self.al_atoms = bulk("Al")
         self.cutoff = 3.0
 
-
     def setUp(self):
         self.pm = PermutationMatrix(self.al_atoms, self.cutoff)
         self.lattice_sites = self.pm.pm_lattice_sites
-
 
     def test_type_of_pm_lattice_sites(self):
         """
@@ -45,13 +42,11 @@ class TestPermutationMatrix(unittest.TestCase):
         self.assertTrue(isinstance(self.lattice_sites, list))
         self.assertEqual(self.lattice_sites.__class__.__name__, 'list')
 
-
     def test_len_of_pm_lattice_sites(self):
         """
         Test that the permutation matrix has the correct length
         """
         self.assertEqual(13, len(self.lattice_sites))
-
 
     def test_type_of_pm_lattice_sites_row(self):
         """
@@ -60,19 +55,16 @@ class TestPermutationMatrix(unittest.TestCase):
         self.assertTrue(isinstance(self.lattice_sites[0], list))
         self.assertEqual(self.lattice_sites[0].__class__.__name__, 'list')
 
-
     def test_len_of_pm_lattice_sites_row(self):
         """
         Test that a row has the correct length
         """
         self.assertEqual(48, len(self.lattice_sites[0]))
 
-
     def test_lattice_sites_element(self):
         """ Test that first element in the permutation matrix is correct"""
         site = self.lattice_sites[0][0]
         self.assertTrue(isinstance(site, LatticeSite))
-
 
     def test_lattice_sites_all_rows(self):
         """
