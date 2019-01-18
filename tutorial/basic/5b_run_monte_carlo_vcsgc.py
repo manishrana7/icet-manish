e8356aa2fd50baa2f393395b9d4f1b80b630ca07
Magnus Rahm <rmagnus@chalmers.se>
2019-01-18 13:59:22 +0000

    Resolve "Tutorial on parallelizing Monte Carlo simulations"

--- tutorial/basic/5b_run_monte_carlo_vcsgc.py
+++ tutorial/basic/5b_run_monte_carlo_vcsgc.py
@@ -2,17 +2,16 @@ from ase.build import make_supercell
 from icet import ClusterExpansion
 from mchammer.calculators import ClusterExpansionCalculator
 from mchammer.ensembles import VCSGCEnsemble
-from numpy import arange, array
+import numpy as np
 from os import mkdir
 
 # step 1: Set up structure to simulate as well as calculator
 ce = ClusterExpansion.read('mixing_energy.ce')
-chemical_symbols = ce.cluster_space.chemical_symbols[0]
 atoms = make_supercell(ce.cluster_space.primitive_structure,
-                       3 * array([[-1, 1, 1],
-                                  [1, -1, 1],
-                                  [1, 1, -1]]))
-atoms.set_chemical_symbols([chemical_symbols[0]] * len(atoms))
+                       3 * np.array([[-1, 1, 1],
+                                     [1, -1, 1],
+                                     [1, 1, -1]]))
+atoms.set_chemical_symbols(['Ag'] * len(atoms))
 calculator = ClusterExpansionCalculator(atoms, ce)
 
 # step 2: Carry out Monte Carlo simulations
@@ -24,7 +23,7 @@ except FileExistsError:
     pass
 for temperature in [900, 300]:
     # Evolve configuration through the entire composition range
-    for phi in arange(-2.1, 0.11, 0.08):
+    for phi in np.arange(-2.1, 0.11, 0.08):
         # Initialize MC ensemble
         mc = VCSGCEnsemble(
             atoms=atoms,
@@ -32,7 +31,7 @@ for temperature in [900, 300]:
             temperature=temperature,
             data_container='{}/vcsgc-T{}-phi{:+.3f}.dc'
                            .format(output_directory, temperature, phi),
-            phis={chemical_symbols[0]: -2.0 - phi, chemical_symbols[1]: phi},
+            phis={'Ag': -2.0 - phi, 'Pd': phi},
             kappa=200)
 
         mc.run(number_of_trial_steps=len(atoms) * 100)
