b9d2e126fb8e362128da60bd4ca0883971b38dd5
angqvist <angqvist@chalmers.se>
2019-02-20 10:34:23 +0100

    #296 added observers to init

--- mchammer/observers/__init__.py
+++ mchammer/observers/__init__.py
@@ -1,3 +1,4 @@
 from .cluster_expansion_observer import ClusterExpansionObserver
 from .cluster_count_observer import ClusterCountObserver
-__all__ = ['ClusterExpansionObserver', 'ClusterCountObserver']
+from .short_range_order_observer import ShortRangeOrderObserver
+__all__ = ['ClusterExpansionObserver', 'ClusterCountObserver','ShortRangeOrderObserver']
