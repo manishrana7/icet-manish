533bbde163054b06f37235dbd4ad6d20f3fb36b5
angqvist <angqvist@chalmers.se>
2019-03-21 11:19:39 +0100

    #296 added proper test for possible orbit decorations

--- tests/unittest/test_icet/core/test_cluster_space.py
+++ tests/unittest/test_icet/core/test_cluster_space.py
@@ -355,8 +355,24 @@ index | order |  radius  | multiplicity | orbit_index | multi_component_vector
     def test_get_possible_orbit_decorations(self):
         """Tests get possible orbit decorations."""
 
-        decorations = self.cs.get_possible_orbit_decorations(orbit_index=3)
-        print(decorations)
+        decoration_singlet = self.cs.get_possible_orbit_decorations(
+            orbit_index=0)
+        self.assertEqual(decoration_singlet, [('Ag',), ('Au',)])
+
+        decoration_pair = self.cs.get_possible_orbit_decorations(orbit_index=1)
+        self.assertEqual(decoration_pair, [
+                         ('Ag', 'Ag'), ('Ag', 'Au'), ('Au', 'Ag'), ('Au', 'Au')])
+
+        decoration_triplet = self.cs.get_possible_orbit_decorations(
+            orbit_index=2)
+        self.assertEqual(decoration_triplet, [('Ag', 'Ag', 'Ag'),
+                                              ('Ag', 'Ag', 'Au'),
+                                              ('Ag', 'Au', 'Ag'),
+                                              ('Ag', 'Au', 'Au'),
+                                              ('Au', 'Ag', 'Ag'),
+                                              ('Au', 'Ag', 'Au'),
+                                              ('Au', 'Au', 'Ag'),
+                                              ('Au', 'Au', 'Au')])
 
 
 class TestClusterSpaceTernary(unittest.TestCase):
@@ -391,10 +407,9 @@ class TestClusterSpaceTernary(unittest.TestCase):
             The orbit which the mc vectors should be returned from.
         """
         orbit = cluster_space.get_orbit(orbit_index)
-        local_Mi = \
-            cluster_space.get_number_of_allowed_species_by_site(
-                cluster_space._get_primitive_structure(),
-                orbit.representative_sites)
+        local_Mi = cluster_space.get_number_of_allowed_species_by_site(
+            cluster_space._get_primitive_structure(),
+            orbit.representative_sites)
 
         mc_vectors = orbit.get_mc_vectors(local_Mi)
         return mc_vectors
