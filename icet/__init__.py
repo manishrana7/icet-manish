e655c251d1e9fd38c29d8ced2bcf46512799b0d1
Paul Erhart <erhart@chalmers.se>
2019-04-05 08:42:32 +0200

    Updated version in __init__.py

--- icet/__init__.py
+++ icet/__init__.py
@@ -26,7 +26,7 @@ __authors__ = ['Mattias Ångqvist',
                'Paul Erhart']
 __copyright__ = '2019'
 __license__ = 'Mozilla Public License 2.0 (MPL 2.0)'
-__version__ = '0.3'
+__version__ = '0.4'
 __all__ = ['ClusterSpace',
            'ClusterExpansion',
            'Structure',
