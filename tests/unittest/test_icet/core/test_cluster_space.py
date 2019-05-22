2c957e6e110bd51c8fb554789287dc5c0cc35bb7
angqvist <angqvist@chalmers.se>
2019-05-22 09:08:28 +0200

    #271 removed debug prints

--- tests/unittest/test_icet/core/test_cluster_space.py
+++ tests/unittest/test_icet/core/test_cluster_space.py
@@ -508,11 +508,8 @@ class TestClusterSpaceTernary(unittest.TestCase):
                                [[0, 1, 2], [0, 2, 1], [2, 0, 1]],
                                [[0, 1, 2], [1, 0, 2], [1, 2, 0]],
                                [[0, 1, 2]]]
-        print("\n\nTesting cluster space on branch 271 (Python)")
         permutation_retval = self.cs.get_multi_component_vector_permutations(
             mc_vector_target, orbit_index)
-        # for l in permutation_retval:
-        #     print(l)
         self.assertEqual(permutations_target, permutation_retval)
 
         # Test orbit 3
