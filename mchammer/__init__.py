1ee91cb4c7ff7bffce02e2dd4fa3da952a3a6c29
Paul Erhart <erhart@chalmers.se>
2019-11-21 11:10:44 +0100

    Release 1.1

--- mchammer/__init__.py
+++ mchammer/__init__.py
@@ -10,24 +10,9 @@ mchammer - Monte Carlo simulation module
 
 __project__ = 'icet-mchammer'
 __description__ = 'icet Monte Carlo simulations module'
-__authors__ = ['Mattias Ångqvist',
-               'William A. Muñoz',
-               'J. Magnus Rahm',
-               'Erik Fransson',
-               'Céline Durniak',
-               'Piotr Rozyczko',
-               'Thomas Holm Rod',
-               'Paul Erhart']
-__copyright__ = '2019'
-__license__ = 'Mozilla Public License 2.0 (MPL 2.0)'
-__version__ = '1.0'
 __all__ = ['ConfigurationManager',
            'DataContainer',
            'WangLandauDataContainer']
 __maintainer__ = 'The icet developers team'
 __maintainer_email__ = 'icet@materialsmodeling.org'
-__status__ = 'beta-version'
 __url__ = 'http://icet.materialsmodeling.org/'
-
-__all__ = ['ConfigurationManager',
-           'DataContainer']
