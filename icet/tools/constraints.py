b717293f8feac3652332b922cd2285ad8cb9b25f
Erik Fransson <karl.erik.fransson@gmail.com>
2020-02-25 12:30:30 +0100

    remove rhs things

--- icet/tools/constraints.py
+++ icet/tools/constraints.py
@@ -5,10 +5,7 @@ from scipy.linalg import null_space
 class Constraints:
 
     def __init__(self, n_params):
-        self._n_params = n_params
-        self.M = np.zeros((1, n_params))
-        self.rhs = [0]
-        self.constraint_vectors = np.eye(self._n_params)
+        self.M = np.empty((0, n_params))
 
     def transform(self, A):
         return A.dot(self.constraint_vectors)
@@ -16,37 +13,34 @@ class Constraints:
     def inverse_transform(self, A):
         return self.constraint_vectors.dot(A)
 
-    def add_constraint(self, M, rhs):
-        if not np.allclose(rhs, 0):
-            raise ValueError('Cant handle rhs != 0')
-
+    def add_constraint(self, M):
         # add constraints
         self.M = np.vstack((self.M, M))
-        self.rhs.extend(rhs)
-
-        # find new constraint vectors
         self.constraint_vectors = null_space(self.M)
 
 
 if __name__ == '__main__':
     from hiphive.fitting import Optimizer
     np.random.seed(42)
-    n, m = 20, 5
+    n, m = 20, 10
 
     A = np.random.random((n, m))
     y = np.random.random(n)
 
-    # constraint eci 2 and 4 to sum to 0
-    M = np.zeros((1, m))
-    M[0, 2], M[0, 4] = 1, 1
+    # constraint sum eci[inds] = 0
+    inds1 = [1, 3, 4, 5]
+    inds2 = [2, 6, 7, 8]
+    M = np.zeros((2, m))
+    M[0, inds1] = 1
+    M[1, inds2] = 1
 
     c = Constraints(m)
-    c.add_constraint(M, [0])
+    c.add_constraint(M)
 
     Ac = c.transform(A)
     opt = Optimizer((Ac, y), fit_method='ridge')
     opt.train()
 
     parameters = c.inverse_transform(opt.parameters)
-    print('eci-2 {:12.8f}'.format(parameters[2]))
-    print('eci-4 {:12.8f}'.format(parameters[4]))
+    print('constraints 1, ', parameters[inds1].sum())
+    print('constraints 2, ', parameters[inds2].sum())
